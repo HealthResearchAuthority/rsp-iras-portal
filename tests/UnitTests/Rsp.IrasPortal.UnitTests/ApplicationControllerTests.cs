ecef7c09daef472babfbec21843da7e655abe74c
Shahzad Hassan <shahzad.hassan@paconsulting.com>
2024-10-02 18:06:56 +0100

    RSP-1521: Isolate Databases
    - Fixed unit test for Application Controller

--- tests/UnitTests/Rsp.IrasPortal.UnitTests/ApplicationControllerTests.cs
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/ApplicationControllerTests.cs
@@ -1,7 +1,11 @@
 ﻿using Bogus;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using Moq.AutoMock;
+using Rsp.IrasPortal.Application.DTOs.Responses;
+using Rsp.IrasPortal.Application.Responses;
+using Rsp.IrasPortal.Application.Services;
 using Rsp.IrasPortal.Web.Controllers;
 using Rsp.IrasPortal.Web.Models;
 using Shouldly;
@@ -22,6 +26,18 @@ public class ApplicationControllerTests
     [Fact]
     public async Task Index_ReturnsViewResult()
     {
+        // Arrange
+        var response = new ServiceResponse<IEnumerable<IrasApplicationResponse>>
+        {
+            Content = [],
+            StatusCode = System.Net.HttpStatusCode.OK
+        };
+
+        var applicationService = _mocker.GetMock<IApplicationsService>();
+        applicationService
+            .Setup(service => service.GetApplications())
+            .ReturnsAsync(response);
+
         // Act
         var result = await _controller.Welcome();
 
