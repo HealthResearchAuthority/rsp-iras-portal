097544a11cbab422f4511daf6b0cd9b5b0d822d3
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-02-19 15:06:46 +0000

    Added ability to save date fields to database

--- src/Web/Rsp.IrasPortal.Web/Models/QuestionViewModel.cs
+++ src/Web/Rsp.IrasPortal.Web/Models/QuestionViewModel.cs
@@ -20,4 +20,40 @@ public class QuestionViewModel
     public string? SelectedOption { get; set; }
     public List<AnswerViewModel> Answers { get; set; } = [];
     public IList<RuleDto> Rules { get; set; } = [];
+
+    private string? _day, _month, _year;
+
+    public string? Day
+    {
+        get => _day;
+        set => SetValue(ref _day, value);
+    }
+
+    public string? Month
+    {
+        get => _month;
+        set => SetValue(ref _month, value);
+    }
+
+    public string? Year
+    {
+        get => _year;
+        set => SetValue(ref _year, value);
+    }
+
+    private void SetValue(ref string? field, string? value)
+    {
+        field = value;
+        UpdateAnswerText();
+    }
+
+    private void UpdateAnswerText()
+    {
+        if (!string.IsNullOrWhiteSpace(Day) &&
+            !string.IsNullOrWhiteSpace(Month) &&
+            !string.IsNullOrWhiteSpace(Year))
+        {
+            AnswerText = $"{Year}-{Month.PadLeft(2, '0')}-{Day.PadLeft(2, '0')}";
+        }
+    }
 }
