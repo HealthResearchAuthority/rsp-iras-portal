b004d710b2a82f9bbf4c360cf3b2cef82ff37814
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-03-16 14:11:36 +0000

    RSP-2570 Ability to create a review body

---
+++ src/Application/Rsp.IrasPortal.Application/DTOs/ReviewBodyDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Rsp.IrasPortal.Application.DTOs;
+
+public class ReviewBodyDto
+{
+    public Guid Id { get; set; }
+    public string OrganisationName { get; set; }
+    public string EmailAddress { get; set; }
+    public List<string> Countries { get; set; } = new();
+    public string Description { get; set; }
+    public bool IsActive { get; set; } = true;
+    public string CreatedBy { get; set; }
+    public string UpdatedBy { get; set; }
+}
---
+++ src/Application/Rsp.IrasPortal.Application/ServiceClients/IReviewBodyServiceClient.cs
@@ -0,0 +1,16 @@
+﻿using Refit;
+using Rsp.IrasPortal.Application.DTOs;
+
+namespace Rsp.IrasPortal.Application.ServiceClients;
+
+/// <summary>
+///     Interface to interact with Applications microservice
+/// </summary>
+public interface IReviewBodyServiceClient
+{
+    /// <summary>
+    ///     Creates a new review body in the database
+    /// </summary>
+    [Post("/reviewbody/create")]
+    public Task<IApiResponse> CreateReviewBody(ReviewBodyDto reviewBodyDto);
+}
