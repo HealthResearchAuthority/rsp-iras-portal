3c11baf349cd3dc4aaf9b879a4c136767996c03c
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-03-16 14:34:06 +0000

    Added custom data attributes for add review body

---
+++ src/Web/Rsp.IrasPortal.Web/Attributes/RequiredListAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class RequiredListAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var list = value as List<string>;
+        if (list == null || !list.Any())
+        {
+            return new ValidationResult(ErrorMessage ?? "Select at least one country.");
+        }
+
+        return ValidationResult.Success;
+    }
+}
