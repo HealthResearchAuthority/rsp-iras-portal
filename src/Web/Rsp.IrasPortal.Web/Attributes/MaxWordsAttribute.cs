0ba6903e4f1ad5e0a4efc07de00fabefd6b97e60
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-03-16 14:34:06 +0000

    Added custom data attributes for add review body

---
+++ src/Web/Rsp.IrasPortal.Web/Attributes/MaxWordsAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rsp.IrasPortal.Web.Attributes;
+
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+public class MaxWordsAttribute : ValidationAttribute
+{
+    private readonly int _maxWords;
+
+    public MaxWordsAttribute(int maxWords, string errorMessage = null)
+    {
+        _maxWords = maxWords;
+        ErrorMessage = errorMessage ?? $"The field can have a maximum of {_maxWords} words.";
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is string str && !string.IsNullOrWhiteSpace(str))
+        {
+            int wordCount = str.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+            if (wordCount > _maxWords)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
--- src/Web/Rsp.IrasPortal.Web/Models/AddUpdateReviewBodyModel.cs
+++ src/Web/Rsp.IrasPortal.Web/Models/AddUpdateReviewBodyModel.cs
@@ -1,18 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Rsp.IrasPortal.Web.Attributes;
 
 namespace Rsp.IrasPortal.Web.Models;
 
 public class AddUpdateReviewBodyModel
 {
     public Guid Id { get; set; }
+
     [Required(ErrorMessage = "Enter the organisation name")]
     public string OrganisationName { get; set; }
+
     [Required(ErrorMessage = "Enter an email address")]
     [EmailAddress(ErrorMessage = "Enter a valid email address")]
     public string EmailAddress { get; set; }
-    public string Description { get; set; }
+
+    [MaxWords(250, ErrorMessage = "The description cannot exceed 250 words.")]
+    public string? Description { get; set; }
+
+    [RequiredList(ErrorMessage = "Select at least one country.")]
     public List<string> Countries { get; set; } = new();
+
     public bool IsActive { get; set; } = true;
-    //public string CreatedBy { get; set; }
-    //public string UpdatedBy { get; set; }
 }
