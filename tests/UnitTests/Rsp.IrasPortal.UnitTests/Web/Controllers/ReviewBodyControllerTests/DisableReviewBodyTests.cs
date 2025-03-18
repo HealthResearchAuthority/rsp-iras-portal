d4fbfbb9de6bde5377dbc164ba36bd2bacfdd760
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-03-18 12:01:06 +0000

    Added Controller Unit Tests

---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Controllers/ReviewBodyControllerTests/DisableReviewBodyTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rsp.IrasPortal.Web.Controllers;
+using Rsp.IrasPortal.Web.Models;
+
+namespace Rsp.IrasPortal.UnitTests.Web.Controllers.ReviewBodyControllerTests;
+
+public class DisableReviewBodyTests : TestServiceBase<ReviewBodyController>
+
+{
+    [Theory]
+    [AutoData]
+    public void DisableReviewBody_WithValidModel_ShouldReturnDisableReviewBodyView(AddUpdateReviewBodyModel model)
+    {
+        // Act
+        var result = Sut.DisableReviewBody(model);
+
+        // Assert
+        var viewResult = result.ShouldBeOfType<ViewResult>();
+        viewResult.ViewName.ShouldBe("SuccessMessage");
+        viewResult.Model.ShouldBeEquivalentTo(model);
+    }
+}
---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Controllers/ReviewBodyControllerTests/SubmitReviewBodyTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rsp.IrasPortal.Application.DTOs;
+using Rsp.IrasPortal.Application.Responses;
+using Rsp.IrasPortal.Application.Services;
+using Rsp.IrasPortal.Web.Controllers;
+using Rsp.IrasPortal.Web.Models;
+
+namespace Rsp.IrasPortal.UnitTests.Web.Controllers.ReviewBodyControllerTests;
+
+public class SubmitReviewBodyTests : TestServiceBase<ReviewBodyController>
+{
+    [Theory]
+    [AutoData]
+    public async Task SubmitReviewBody_WithValidData_ShouldReturnSuccessView(
+        AddUpdateReviewBodyModel model)
+    {
+        // Arrange
+        var serviceResponse = new ServiceResponse<IEnumerable<ReviewBodyDto>>
+        {
+            StatusCode = HttpStatusCode.OK
+        };
+
+        model.Id = Guid.Empty;
+
+        Mocker.GetMock<IReviewBodyService>()
+            .Setup(s => s.CreateReviewBody(It.IsAny<ReviewBodyDto>()))
+            .ReturnsAsync(serviceResponse);
+
+        // Act
+        var result = await Sut.SubmitReviewBody(model);
+
+        // Assert
+        var viewResult = result.ShouldBeOfType<ViewResult>();
+        viewResult.ViewName.ShouldBe("SuccessMessage");
+        viewResult.Model.ShouldBeEquivalentTo(model);
+
+        // Verify the service method was called once
+        Mocker.GetMock<IReviewBodyService>()
+            .Verify(s => s.CreateReviewBody(It.IsAny<ReviewBodyDto>()), Times.Once);
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task SubmitReviewBody_WithInvalidData_ShouldReturnSuccessView(
+        AddUpdateReviewBodyModel model)
+    {
+        // Arrange
+        var serviceResponse = new ServiceResponse<IEnumerable<ReviewBodyDto>>
+        {
+            StatusCode = HttpStatusCode.InternalServerError
+        };
+
+        model.Id = Guid.Empty;
+
+        Mocker.GetMock<IReviewBodyService>()
+            .Setup(s => s.CreateReviewBody(It.IsAny<ReviewBodyDto>()))
+            .ReturnsAsync(serviceResponse);
+
+        // Act
+        var result = await Sut.SubmitReviewBody(model);
+
+        // Assert
+        var viewResult = result.ShouldBeOfType<ViewResult>();
+        viewResult.ViewName.ShouldBe("SuccessMessage");
+        viewResult.Model.ShouldBeEquivalentTo(model);
+
+        // Verify the service method was called once
+        Mocker.GetMock<IReviewBodyService>()
+            .Verify(s => s.CreateReviewBody(It.IsAny<ReviewBodyDto>()), Times.Once);
+    }
+}
