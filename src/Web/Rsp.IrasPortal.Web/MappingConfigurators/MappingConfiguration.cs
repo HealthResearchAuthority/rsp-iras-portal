71ede5cb6ced8b6cddb4d4eb260c7717d6d2ffc4
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-03-19 09:27:12 +0000

    - add disable user functionality - tidy up model mapping by using mapster - add pagination to list users

---
+++ src/Web/Rsp.IrasPortal.Web/MappingConfigurators/MappingConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Mapster;
+using Rsp.IrasPortal.Application.DTOs.Requests.UserManagement;
+using Rsp.IrasPortal.Web.Areas.Admin.Models;
+
+namespace Rsp.IrasPortal.Web.MappingConfigurators;
+
+public static class MappingConfiguration
+{
+    public static void Configure()
+    {
+        TypeAdapterConfig<UserViewModel, UpdateUserRequest>
+            .NewConfig()
+            .Map(dest => dest.Country, src => src.Country != null ? string.Join(',', src.Country) : null);
+
+        TypeAdapterConfig<UserViewModel, CreateUserRequest>
+            .NewConfig()
+            .Map(dest => dest.Country, src => src.Country != null ? string.Join(',', src.Country) : null);
+    }
+}
