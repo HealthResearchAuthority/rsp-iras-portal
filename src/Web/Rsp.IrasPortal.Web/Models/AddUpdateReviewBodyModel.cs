1b3d53068f0fd5515cbf87b564921cacc8f326b0
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-03-19 17:27:08 +0000

    - remove mapping configurator unit test - change Mapster configuration registry method - rename MappingConfigurators folder to Mapping

--- src/Web/Rsp.IrasPortal.Web/Models/AddUpdateReviewBodyModel.cs
+++ src/Web/Rsp.IrasPortal.Web/Models/AddUpdateReviewBodyModel.cs
@@ -6,7 +6,7 @@ public class AddUpdateReviewBodyModel
     public string OrganisationName { get; set; } = null!;
     public string EmailAddress { get; set; } = null!;
     public string? Description { get; set; }
-    public List<string> Countries { get; set; } 
+    public List<string> Countries { get; set; } = null!;
     public bool IsActive { get; set; } = true;
     public DateTime UpdatedDate { get; set; }
 }
