2899bbe102ac236ff15d04b7acb61fd6776328c0
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-03-20 16:14:18 +0000

    - resolve QA flags

--- src/Application/Rsp.IrasPortal.Application/Constants/AccessRequiredOptions.cs
+++ src/Application/Rsp.IrasPortal.Application/Constants/AccessRequiredOptions.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Rsp.IrasPortal.Application.Constants;
+﻿namespace Rsp.IrasPortal.Application.Constants;
 
 public static class AccessRequiredOptions
 {
-    public static readonly string[] Options = { "Study Wide Review", "Research Ethics Comittee", "CAG", "Technical Assurance" };
+    public static readonly string[] Options = { "Study Wide Review", "Research Ethics Committee", "CAG", "Technical Assurance" };
 }
