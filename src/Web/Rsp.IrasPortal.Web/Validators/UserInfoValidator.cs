471d24a7ca8cdf7d676a837adf22dbc3aed0557b
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-03-20 16:14:18 +0000

    - resolve QA flags

--- src/Web/Rsp.IrasPortal.Web/Validators/UserInfoValidator.cs
+++ src/Web/Rsp.IrasPortal.Web/Validators/UserInfoValidator.cs
@@ -27,11 +27,14 @@ public class UserInfoValidator : AbstractValidator<UserViewModel>
             .MaximumLength(250)
             .WithMessage(MaxCharactersErrorMessage);
 
+        // email validation to loosley comply with RFC 5322 standard
         RuleFor(x => x.Email)
             .NotEmpty()
             .WithMessage(MandatoryErrorMessage)
-            .EmailAddress()
-            .WithMessage("Incorrect email format");
+            .MaximumLength(255)
+            .WithMessage("Max 255 characters allowed")
+            .Matches(@"^(?!(?:(?:.*\.\.)|(?:.*\.\@)))(?!.*\.\.$)(?!.*\.\@)[\p{L}\p{N}!#$%&'*+/=?^_`{|}~.-]+@[\p{L}\p{N}.-]+\.[\p{L}]{2,}$")
+            .WithMessage("Invalid email format.");
 
         RuleFor(x => x.Telephone)
             .MaximumLength(11)
@@ -44,10 +47,6 @@ public class UserInfoValidator : AbstractValidator<UserViewModel>
         RuleFor(x => x.JobTitle)
             .MaximumLength(250)
             .WithMessage(MaxCharactersErrorMessage);
-
-        RuleFor(x => x.Role)
-            .NotEmpty()
-            .WithMessage(MandatoryErrorMessage);
 
         RuleFor(x => x.Country)
             .NotEmpty()
