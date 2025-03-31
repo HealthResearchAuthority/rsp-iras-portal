114acaf41136480a5fee1372ab2db414c9315492
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-03-31 11:53:14 +0100

    RSP-2929: Enable disabled review body

---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Services/ReviewBodyServiceTests/EnableReviewBodyTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Rsp.IrasPortal.Application.DTOs;
+using Rsp.IrasPortal.Application.Responses;
+using Rsp.IrasPortal.Application.ServiceClients;
+using Rsp.IrasPortal.Services;
+
+namespace Rsp.IrasPortal.UnitTests.Services.ReviewBodyServiceTests;
+
+public class EnableReviewBodyTests : TestServiceBase<ReviewBodyService>
+{
+    [Theory]
+    [AutoData]
+    public async Task EnableReviewBody_Should_Return_Success(
+        ReviewBodyDto reviewBodyDto)
+    {
+        // Arrange
+        // Arrange
+        var apiResponse = Mock.Of<IApiResponse<ReviewBodyDto>>(
+            apiResponse => apiResponse.IsSuccessStatusCode &&
+                           apiResponse.StatusCode == HttpStatusCode.OK &&
+                           apiResponse.Content == reviewBodyDto);
+
+
+        var client = new Mock<IReviewBodyServiceClient>();
+        client
+            .Setup(c => c.EnableReviewBody(It.IsAny<Guid>()))
+            .ReturnsAsync(apiResponse);
+
+        var sut = new ReviewBodyService(client.Object);
+
+        // Act
+        var result = await sut.EnableReviewBody(reviewBodyDto.Id);
+
+        // Assert
+        result.ShouldBeOfType<ServiceResponse<ReviewBodyDto>>();
+        result.IsSuccessStatusCode.ShouldBeTrue();
+
+        // Verify
+        client.Verify(c => c.EnableReviewBody(reviewBodyDto.Id), Times.Once());
+    }
+}
---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Controllers/ReviewBodyControllerTests/ConfirmStatusUpdateTests.cs
@@ -0,0 +1,70 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Rsp.IrasPortal.Application.DTOs;
+using Rsp.IrasPortal.Application.Responses;
+using Rsp.IrasPortal.Application.Services;
+using Rsp.IrasPortal.Web.Controllers;
+using Rsp.IrasPortal.Web.Models;
+
+namespace Rsp.IrasPortal.UnitTests.Web.Controllers.ReviewBodyControllerTests;
+
+public class ConfirmStatusUpdateTests : TestServiceBase<ReviewBodyController>
+
+{
+    [Theory, AutoData]
+    public async Task ConfirmStatusUpdate_DisableReviewBody(List<ReviewBodyDto> reviewBodies,
+        AddUpdateReviewBodyModel addUpdateReviewBodyModel)
+    {
+        // Arrange
+        var serviceResponse = new ServiceResponse<IEnumerable<ReviewBodyDto>>
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = reviewBodies
+        };
+
+        Mocker.GetMock<IReviewBodyService>()
+            .Setup(s => s.GetReviewBodyById(It.IsAny<Guid>()))
+            .ReturnsAsync(serviceResponse);
+
+        Mocker.GetMock<IReviewBodyService>()
+            .Setup(s => s.DisableReviewBody(It.IsAny<Guid>()))
+            .ReturnsAsync(serviceResponse);
+
+        // Act
+        var result = await Sut.ConfirmStatusUpdate(addUpdateReviewBodyModel);
+
+        // Assert
+        var viewResult = result.ShouldBeOfType<ViewResult>();
+        viewResult.Model.ShouldBeAssignableTo<AddUpdateReviewBodyModel>();
+    }
+
+    [Theory, AutoData]
+    public async Task ConfirmStatusUpdate_EnableReviewBody(List<ReviewBodyDto> reviewBodies,
+        AddUpdateReviewBodyModel addUpdateReviewBodyModel)
+    {
+        // Arrange
+        var serviceResponse = new ServiceResponse<IEnumerable<ReviewBodyDto>>
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = reviewBodies
+        };
+
+        Mocker.GetMock<IReviewBodyService>()
+            .Setup(s => s.GetReviewBodyById(It.IsAny<Guid>()))
+            .ReturnsAsync(serviceResponse);
+
+        Mocker.GetMock<IReviewBodyService>()
+            .Setup(s => s.DisableReviewBody(It.IsAny<Guid>()))
+            .ReturnsAsync(serviceResponse);
+
+        addUpdateReviewBodyModel.IsActive = true;
+
+        // Act
+        var result = await Sut.ConfirmStatusUpdate(addUpdateReviewBodyModel);
+
+        // Assert
+        var viewResult = result.ShouldBeOfType<ViewResult>();
+        viewResult.Model.ShouldBeAssignableTo<AddUpdateReviewBodyModel>();
+    }
+}
