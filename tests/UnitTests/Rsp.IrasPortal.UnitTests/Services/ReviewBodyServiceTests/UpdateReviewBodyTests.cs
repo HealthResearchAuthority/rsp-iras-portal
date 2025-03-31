017c220722b9f3f795b35278a31eb6c6e86819cf
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-03-31 09:15:46 +0100

    Added unit test coverage

---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Services/ReviewBodyServiceTests/UpdateReviewBodyTests.cs
@@ -0,0 +1,71 @@
+﻿using Rsp.IrasPortal.Application.DTOs;
+using Rsp.IrasPortal.Application.Responses;
+using Rsp.IrasPortal.Application.ServiceClients;
+using Rsp.IrasPortal.Services;
+
+namespace Rsp.IrasPortal.UnitTests.Services.ReviewBodyServiceTests;
+
+public class UpdateReviewBodyTests : TestServiceBase<ReviewBodyService>
+{
+    [Theory,AutoData]
+    public async Task UpdateReviewBody_Should_Return_Failure_Response_When_Client_Returns_Failure(
+        ReviewBodyDto reviewBodyDto)
+    {
+        // Arrange
+        var apiResponse = Mock.Of<IApiResponse>
+        (
+            apiResponse =>
+                !apiResponse.IsSuccessStatusCode &&
+                apiResponse.StatusCode == HttpStatusCode.BadRequest
+        );
+
+        var client = new Mock<IReviewBodyServiceClient>();
+        client
+            .Setup(c => c.UpdateReviewBody(reviewBodyDto))
+            .ReturnsAsync(apiResponse);
+
+        var sut = new ReviewBodyService(client.Object);
+
+        // Act
+        var result = await sut.UpdateReviewBody(reviewBodyDto);
+
+        // Assert
+        result.ShouldBeOfType<ServiceResponse>();
+        result.IsSuccessStatusCode.ShouldBeFalse();
+        result.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+        // Verify
+        client.Verify(c => c.UpdateReviewBody(reviewBodyDto), Times.Once());
+    }
+
+    [Theory,AutoData]
+    public async Task UpdateReviewBody_Should_Return_Success_Response_When_Client_Returns_Success(
+        ReviewBodyDto reviewBodyDto)
+    {
+        // Arrange
+        var apiResponse = Mock.Of<IApiResponse>
+        (
+            apiResponse =>
+                apiResponse.IsSuccessStatusCode &&
+                apiResponse.StatusCode == HttpStatusCode.OK
+        );
+
+        var client = new Mock<IReviewBodyServiceClient>();
+        client
+            .Setup(c => c.UpdateReviewBody(reviewBodyDto))
+            .ReturnsAsync(apiResponse);
+
+        var sut = new ReviewBodyService(client.Object);
+
+        // Act
+        var result = await sut.UpdateReviewBody(reviewBodyDto);
+
+        // Assert
+        result.ShouldBeOfType<ServiceResponse>();
+        result.IsSuccessStatusCode.ShouldBeTrue();
+        result.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+        // Verify
+        client.Verify(c => c.UpdateReviewBody(reviewBodyDto), Times.Once());
+    }
+}
