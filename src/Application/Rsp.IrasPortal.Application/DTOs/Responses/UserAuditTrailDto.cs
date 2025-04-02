f85f89cf581a8322b529346d39b77e1af9abf8ff
Haris Amin <haris.amin_PA@hra.nhs.uk>
2025-04-02 14:34:01 +0100

    RSP-2928: Manage users audit log

---
+++ src/Application/Rsp.IrasPortal.Application/DTOs/Responses/UserAuditTrailDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Rsp.IrasPortal.Application.DTOs.Responses;
+
+public class UserAuditTrailDto
+{
+    public DateTime DateTimeStamp { get; set; }
+    public string Description { get; set; } = null!;
+    public string SystemAdmin { get; set; } = null!;
+}
