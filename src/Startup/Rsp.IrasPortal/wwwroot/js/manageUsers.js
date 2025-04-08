02424659bd6b10de193ae6002f7232f2c60ad761
Vinay Lingappa <vinay.lingappa_PA@hra.nhs.uk>
2025-04-08 14:57:52 +0100

    - manage users show conditional items for operations

--- src/Startup/Rsp.IrasPortal/wwwroot/js/manageUsers.js
+++ src/Startup/Rsp.IrasPortal/wwwroot/js/manageUsers.js
@@ -2,6 +2,35 @@
 
 // Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+$(function () {
+    function toggleConditionalField() {
+        let shouldShow = false;
+
+        $('.govuk-checkboxes__item').each(function () {
+            const checkbox = $(this).find('input[type="checkbox"]');
+            const labelText = $(this).find('label').text().trim();
+            const isChecked = checkbox.is(':checked');
+
+            if (labelText.toLowerCase() === 'operations' && isChecked) {
+                shouldShow = true;
+            }
+        });
+
+        if (shouldShow) {
+            $('.conditional-field').show();
+        } else {
+            $('.conditional-field').hide();
+        }
+    }
+
+    // Initial check on page load
+    toggleConditionalField();
+
+    // Bind change event to all checkboxes
+    $('.govuk-checkboxes__input').on('change', function () {
+        toggleConditionalField();
+    });
+});
 
 $(function () {
     // Hide all conditional questions initially if the selected role is 'operations'
@@ -36,7 +65,6 @@ function changeRoleSelection(role) {
         $(".conditional-field").hide();
     }
 }
-
 
 function submitFormWithAction(formId, url) {
     let form = document.getElementById(formId);
