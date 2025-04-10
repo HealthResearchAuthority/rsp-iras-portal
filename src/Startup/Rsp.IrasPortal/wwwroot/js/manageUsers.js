466f1e8f6148f372aa1be889f643bb047ef7a68d
Shahzad Hassan <shahzad.hassan@paconsulting.com>
2025-04-10 23:55:54 +0100

    - optimize the jQuery code for manageUsers.js so   it only looks for the UserRoles checkboxes and   associated label. - modified EditUserView.cshtml to correctly   associate the label with the checkbox

--- src/Startup/Rsp.IrasPortal/wwwroot/js/manageUsers.js
+++ src/Startup/Rsp.IrasPortal/wwwroot/js/manageUsers.js
@@ -4,21 +4,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 $(function () {
     function toggleConditionalField() {
-        let shouldShow = false;
+        let showConditionalFields = false;
 
-        $('.govuk-checkboxes__item').each(function () {
-            const checkbox = $(this).find('input[type="checkbox"]');
-            const labelText = $(this).find('label').text().trim();
-            const isChecked = checkbox.is(':checked');
+        // get all the checked checkboxes with name starting with UserRoles
+        $("input:checked[name^='UserRoles']").each(function () {
+            // get the label for the checkbox
+            const label = $("label[for='" + $(this).attr('id') + "']");
 
-            if (labelText.toLowerCase() === 'operations' && isChecked) {
-                shouldShow = true;
+            // check if the label text is 'Operations'
+            if (label.text() === 'operations') {
+                $(".conditional-field").show();
+                showConditionalFields = true;
+                return false;
             }
         });
 
-        if (shouldShow) {
-            $(".conditional-field").show();
-        } else {
+        // if no checkbox with label 'Operations' is checked, hide the conditional field
+        if (!showConditionalFields) {
             $(".conditional-field").hide();
         }
     }
@@ -26,17 +28,11 @@ $(function () {
     // Initial check on page load
     toggleConditionalField();
 
-    // Bind change event to all checkboxes
-    $('.govuk-checkboxes__input').on('change', function () {
+    // Bind change event to all checkboxes with name starting with UserRoles
+    $("input[type='checkbox'][name^='UserRoles']").on('change', function () {
         toggleConditionalField();
     });
 });
-
-/**
- * Updates the visibility of a conditional question based on rule evaluation.
- * @param {string} questionId - The ID of the conditional question.
- * @param {jQuery} $conditionalElement - The jQuery object for the conditional question element.
- */
 
 function submitFormWithAction(formId, url) {
     let form = document.getElementById(formId);
