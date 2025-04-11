bf6e74217800e948af6a4ffd8dc3ade35c96d274
Vinay Lingappa <vinay.lingappa_PA@hra.nhs.uk>
2025-04-11 16:52:22 +0100

    - duplicate IRAS ID check

---
+++ src/Web/Rsp.IrasPortal.Web/Models/IrasIdCheckViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rsp.IrasPortal.Web.Models;
+
+public class IrasIdCheckViewModel
+{
+    public string? IrasId { get; set; }
+}
---
+++ src/Web/Rsp.IrasPortal.Web/Validators/IrasIdCheckValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Rsp.IrasPortal.Web.Models;
+
+namespace Rsp.IrasPortal.Web.Validators;
+
+public class IrasIdCheckValidator : AbstractValidator<IrasIdCheckViewModel>
+{
+    public IrasIdCheckValidator()
+    {
+        // Validate IRAS ID
+        RuleFor(x => x.IrasId)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+                .WithMessage("Enter an IRAS ID")
+            .Length(4, 7)
+                .WithMessage("IRAS ID must be 4 to 7 digits")
+            .Matches(@"^\d+$")
+                .WithMessage("IRAS ID must only contain numbers");
+    }
+}
