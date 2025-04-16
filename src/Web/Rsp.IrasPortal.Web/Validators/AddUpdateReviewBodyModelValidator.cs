40c8aeea8aeaeaa7b16467f81a40c74ed53350b9
Haris Amin <haris.amin_PA@hra.nhs.uk>
2025-04-16 14:58:20 +0100

    RSP-3123: Manage review bodies validation error messages

--- src/Web/Rsp.IrasPortal.Web/Validators/AddUpdateReviewBodyModelValidator.cs
+++ src/Web/Rsp.IrasPortal.Web/Validators/AddUpdateReviewBodyModelValidator.cs
@@ -1,48 +1,58 @@
-﻿using System.Globalization;
-using System.Net;
-using System.Text.RegularExpressions;
-using FluentValidation;
+﻿using FluentValidation;
 using Rsp.IrasPortal.Web.Models;
 
 namespace Rsp.IrasPortal.Web.Validators;
 
 public class AddUpdateReviewBodyModelValidator : AbstractValidator<AddUpdateReviewBodyModel>
 {
-    private const string MandatoryErrorMessage = "Field is mandatory";
+    private const int MaxWordCount = 250;
 
     public AddUpdateReviewBodyModelValidator()
     {
         RuleFor(x => x.OrganisationName)
-            .NotEmpty().WithMessage(MandatoryErrorMessage)
-            .MaximumLength(250).WithMessage("Max 250 characters allowed");
+            .NotEmpty()
+            .WithMessage("Enter an organisation name")
+            .MaximumLength(250)
+            .WithMessage("Organisation name must be 250 characters or less");
+
+        RuleFor(x => x.Countries)
+            .Must(c => c?.Count > 0)
+            // This error message is matching the businesss requirements but is not correct as per
+            // the GDS style guide. The correct error messages are shown under the 'Error messages'
+            // section on this page: https://design-system.service.gov.uk/components/checkboxes/
+            .WithMessage("Enter a country");
 
         RuleFor(x => x.EmailAddress)
             .NotEmpty()
-            .WithMessage(MandatoryErrorMessage)
-            .MaximumLength(255)
-            .WithMessage("Max 255 characters allowed")
+            .WithMessage("Enter an email address")
+            .MaximumLength(250)
+            .WithMessage("Email address must be 250 characters or less")
             .Matches(@"^(?!(?:(?:.*\.\.)|(?:.*\.\@)))(?!.*\.\.$)(?!.*\.\@)[\p{L}\p{N}!#$%&'*+/=?^_`{|}~.-]+@[\p{L}\p{N}.-]+\.[\p{L}]{2,}$")
-            .WithMessage("Invalid email format");
-
+            .WithMessage("Enter an email address in the correct format");
 
         RuleFor(x => x.Description)
-            .Must(text => HaveMaxWords(text, 500))
-            .WithMessage("The description cannot exceed 500 words.")
-            .When(x => !string.IsNullOrWhiteSpace(x.Description));
+            .Must(text => GetWordCount(text) <= MaxWordCount)
+            .WithMessage($"The description must be {MaxWordCount} words or less");
 
-        RuleFor(x => x.Countries)
-            .Must(c => c != null && c.Any())
-            .WithMessage("Select at least one country.");
+        RuleFor(x => x.Description)
+            .Custom((text, context) =>
+            {
+                var wordCount = GetWordCount(text);
+                if (wordCount > MaxWordCount)
+                {
+                    var excessWords = wordCount - MaxWordCount;
+                    context.AddFailure("_DescriptionExcessWordCount", $"You have {excessWords} word{(excessWords == 1 ? "" : "s")} too many");
+                }
+            });
     }
 
-    private static bool HaveMaxWords(string? text, int maxWords)
+    private static int GetWordCount(string? text)
     {
         if (string.IsNullOrWhiteSpace(text))
         {
-            return true;
+            return 0;
         }
-
         var words = text.Split([' ', '\t', '\n', '\r'], StringSplitOptions.RemoveEmptyEntries);
-        return words.Length <= maxWords;
+        return words.Length;
     }
 }
