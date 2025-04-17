41a39f7aa2bdd7c58d66d4c8910877737e244e1d
Jack Packham <Jack.Packham_PA@hra.nhs.uk>
2025-04-17 13:54:40 +0100

    Removed neccesity for constructor for ProjectOverviewModel

--- src/Web/Rsp.IrasPortal.Web/Models/ProjectOverviewModel.cs
+++ src/Web/Rsp.IrasPortal.Web/Models/ProjectOverviewModel.cs
@@ -2,16 +2,9 @@
 
 public class ProjectOverviewModel
 {
-    public string ProjectTitle { get; set; }
+    public string? ProjectTitle { get; set; }
 
-    public string CategoryId { get; set; }
+    public string? CategoryId { get; set; }
 
-    public string ApplicationId { get; set; }
-
-    public ProjectOverviewModel(string projectTitle, string categoryId, string applicationId)
-    {
-        ProjectTitle = projectTitle;
-        CategoryId = categoryId;
-        ApplicationId = applicationId;
-    }
+    public string? ApplicationId { get; set; }
 }
