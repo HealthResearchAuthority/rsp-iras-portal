0fdeabde0cf743ca294c4922f995770fc6d2439e
Jack Packham <Jack.Packham_PA@hra.nhs.uk>
2025-04-28 15:50:45 +0100

    Added leading zeros logic to IrasID validation

--- src/Web/Rsp.IrasPortal.Web/Validators/IrasIdViewModelValidator.cs
+++ src/Web/Rsp.IrasPortal.Web/Validators/IrasIdViewModelValidator.cs
@@ -15,6 +15,8 @@ public class IrasIdViewModelValidator : AbstractValidator<IrasIdViewModel>
             .Length(4, 7)
                 .WithMessage("IRAS ID must be 4 to 7 digits")
             .Matches(@"^\d+$")
-                .WithMessage("IRAS ID must only contain numbers");
+                .WithMessage("IRAS ID must only contain numbers")
+            .Must(id => id == null || !id.StartsWith("0"))
+                .WithMessage("IRAS ID cannot start with '0'.");
     }
 }
