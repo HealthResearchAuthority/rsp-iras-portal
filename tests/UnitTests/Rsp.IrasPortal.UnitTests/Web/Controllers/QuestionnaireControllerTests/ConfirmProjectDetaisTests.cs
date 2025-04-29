d96fde6d93500ea7433c6836d21e827c3c12b469
Vinay Lingappa <vinay.lingappa_PA@hra.nhs.uk>
2025-04-29 15:12:35 +0100

    - improving the code coverage for QuestionViewModel

--- tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Controllers/QuestionnaireControllerTests/ConfirmProjectDetaisTests.cs
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Controllers/QuestionnaireControllerTests/ConfirmProjectDetaisTests.cs
@@ -163,7 +163,39 @@ public class ConfirmProjectDetaisTests : TestServiceBase<QuestionnaireController
 
     private void SetupApplicationInSession()
     {
-        var faker = new Faker<QuestionViewModel>();
+        var faker = new Faker<QuestionViewModel>()
+        .RuleFor(q => q.Index, fake => fake.IndexFaker)
+        .RuleFor(q => q.QuestionId, fake => fake.Random.Guid().ToString())
+        .RuleFor(q => q.Category, fake => "project-details")
+        .RuleFor(q => q.SectionId, fake => "section-1")
+        .RuleFor(q => q.Section, fake => "Project Details")
+        .RuleFor(q => q.Sequence, fake => fake.Random.Int(1, 10))
+        .RuleFor(q => q.Heading, fake => fake.Lorem.Sentence())
+        .RuleFor(q => q.QuestionText, fake => "What is the start date of the project?")
+        .RuleFor(q => q.ShortQuestionText, fake => "Project start date")
+        .RuleFor(q => q.DataType, fake => fake.PickRandom("Date", "Text", "Email", "Checkbox", "Boolean", "Radio button"))
+        .RuleFor(q => q.AnswerText, (f, q) =>
+        {
+            return q.DataType == "Date" ? "2026-04-23" :
+                   q.DataType == "Text" ? "sample text" :
+                   null;
+        })
+        .RuleFor(q => q.SelectedOption, (f, q) => q.DataType is "Boolean" or "Radio button" ? "yes" : null)
+        .RuleFor(q => q.Answers, (f, q) =>
+        {
+            var answers = new List<AnswerViewModel>
+            {
+                new() { AnswerId = "yes", AnswerText = "Yes", IsSelected = q.SelectedOption == "yes" },
+                new() { AnswerId = "no", AnswerText = "No", IsSelected = false }
+            };
+
+            if (q.DataType == "Checkbox")
+            {
+                answers.ForEach(a => a.IsSelected = f.Random.Bool());
+            }
+
+            return answers;
+        });
         List<QuestionViewModel> expectedQuestions = faker.Generate(3);
 
         var session = new Mock<ISession>();
