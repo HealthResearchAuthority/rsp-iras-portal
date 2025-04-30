c84918cf09d44fb6fb95a6a48c99283bd367aa05
Shahzad Hassan <shahzad.hassan@paconsulting.com>
2025-04-30 03:18:30 +0100

    RSP-1861: Sponsor Organization Selection - Integrated AutoSuggest functionality into the questionnaire page. - Added new editor templates for various input types (AutoSuggest, Checkbox, Date, RadioButton, Text). - Updated the questionnaire index view to utilize the new templates.

---
+++ src/Startup/Rsp.IrasPortal/wwwroot/js/rts.js
@@ -0,0 +1,58 @@
+﻿/// <reference path="../lib/jquery/dist/jquery.js" />
+
+function initAutocomplete(elementId, inputName, apiUrl) {
+    accessibleAutocomplete({
+        element: document.querySelector('.autocomplete-wrapper'),
+        id: elementId,
+        name: inputName,
+        minLength: 3, // Start suggesting immediately
+        source: [
+            'United Kingdom',
+            'United States',
+            'Canada',
+            'Australia',
+            'India',
+            'Germany',
+            'France'
+        ],
+        templates: {
+            suggestion: function (suggestion) {
+                return suggestion;
+            }
+        }
+    });
+}
+
+//function initAutocomplete(elementId, inputName, apiUrl) {
+//    accessibleAutocomplete({
+//        element: document.querySelector(`#${elementId}-wrapper`),
+//        id: elementId,
+//        name: inputName,
+//        minLength: 3,
+//        source: function (query, populateResults) {
+//            if (query.length < 3) {
+//                populateResults([]);
+//                return;
+//            }
+
+//            $.ajax({
+//                url: apiUrl,
+//                method: 'GET',
+//                data: { query: query },
+//                dataType: 'json',
+//                success: function (data) {
+//                    populateResults(data);
+//                },
+//                error: function (xhr, status, error) {
+//                    console.error('Error fetching suggestions:', error);
+//                    populateResults([]);
+//                }
+//            });
+//        },
+//        templates: {
+//            suggestion: function (suggestion) {
+//                return suggestion;
+//            }
+//        }
+//    });
+//}
