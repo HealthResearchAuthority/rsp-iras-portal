c89250888a97b494e16f678c76a4758b296e8e51
Jack Packham <Jack.Packham_PA@hra.nhs.uk>
2025-05-22 10:35:32 +0100

    Initial bulk commit

---
+++ src/Web/Rsp.IrasPortal.Web/Models/ResearchApplicationSummaryModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Rsp.IrasPortal.Web.Models;
+
+public class ResearchApplicationSummaryModel
+{
+    public int? IrasId { get; set; }
+    public string Title { get; set; } = string.Empty;
+
+    public bool IsNew { get; set; } = false; // temporary for first iteration
+    public DateTime ProjectEndDate { get; set; }
+}
