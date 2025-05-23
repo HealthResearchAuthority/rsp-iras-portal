712a7af12ea64e829aafded88953f68d0971ab8b
Jack Packham <Jack.Packham_PA@hra.nhs.uk>
2025-05-23 11:04:49 +0100

    Added redirect to projectoverview

--- src/Web/Rsp.IrasPortal.Web/Models/ResearchApplicationSummaryModel.cs
+++ src/Web/Rsp.IrasPortal.Web/Models/ResearchApplicationSummaryModel.cs
@@ -3,8 +3,9 @@
 public class ResearchApplicationSummaryModel
 {
     public int? IrasId { get; set; }
-    public string Title { get; set; } = string.Empty;
 
+    public string? ApplicatonId { get; set; }
+    public string Title { get; set; } = string.Empty;
     public bool IsNew { get; set; } = false; // temporary for first iteration
     public DateTime ProjectEndDate { get; set; }
 }
