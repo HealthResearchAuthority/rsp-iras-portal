a26068c00bd71326e29e503c25b1c989c5875d5e
Vinay Lingappa <vinay.lingappa_PA@hra.nhs.uk>
2025-06-24 14:31:06 +0100

    - fixed failing unit test

--- tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Controllers/ApplicationControllerTests/WelcomeTests.cs
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Controllers/ApplicationControllerTests/WelcomeTests.cs
@@ -1,15 +1,31 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Rsp.IrasPortal.Application.Constants;
 using Rsp.IrasPortal.Web.Controllers;
 
 namespace Rsp.IrasPortal.UnitTests.Web.Controllers.ApplicationControllerTests
 {
     public class WelcomeTests : TestServiceBase<ApplicationController>
     {
+        private readonly Mock<ISession> session = new();
+
         [Fact]
-        public void Welcome_ReturnsViewResult_WithIndexViewName()
+        public async Task Welcome_ReturnsViewResult_WithIndexViewName()
         {
+            var httpContext = new DefaultHttpContext
+            {
+                Session = session.Object
+            };
+
+            httpContext.Items[ContextItemKeys.RespondentId] = "RespondentId1";
+
+            Sut.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
+
             // Act
-            var result = Sut.Welcome();
+            var result = await Sut.Welcome();
 
             // Assert
             var viewResult = result.ShouldBeOfType<ViewResult>();
