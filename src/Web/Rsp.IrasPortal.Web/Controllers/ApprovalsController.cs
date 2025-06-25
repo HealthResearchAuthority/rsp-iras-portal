725a8df28844f20887646ad2e02104b125c88556
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-06-25 13:32:07 +0100

    Added approvals index & search pages inc controller

---
+++ src/Web/Rsp.IrasPortal.Web/Controllers/ApprovalsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rsp.IrasPortal.Web.Controllers;
+
+[Route("[controller]/[action]", Name = "approvals:[action]")]
+[Authorize(Policy = "IsUser")]
+public class ApprovalsController : Controller
+{
+    public IActionResult Welcome() => View(nameof(Index));
+    public IActionResult Search() => View(nameof(Search));
+}
