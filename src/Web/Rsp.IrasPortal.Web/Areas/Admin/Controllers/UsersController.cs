7ad814e690ab5fe7c9a49e231c637f06180189c0
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-06-25 12:13:08 +0100

    RSP-3899 Conditional logic errors - Manage users form

--- src/Web/Rsp.IrasPortal.Web/Areas/Admin/Controllers/UsersController.cs
+++ src/Web/Rsp.IrasPortal.Web/Areas/Admin/Controllers/UsersController.cs
@@ -33,6 +33,7 @@ public class UsersController(IUserManagementService userManagementService, IVali
     private const string ConfirmDisableUser = nameof(ConfirmDisableUser);
     private const string EnableUserSuccessMessage = nameof(EnableUserSuccessMessage);
     private const string ConfirmEnableUser = nameof(ConfirmEnableUser);
+    private const string OperationsRole = "operations";
 
     private const string EditMode = "edit";
     private const string CreateMode = "create";
@@ -135,6 +136,12 @@ public class UsersController(IUserManagementService userManagementService, IVali
     {
         ViewBag.Mode = string.IsNullOrEmpty(model.Id) ? CreateMode : EditMode;
 
+        if (model.UserRoles.Any(r => r.Name.Equals(OperationsRole, StringComparison.OrdinalIgnoreCase) && !r.IsSelected))
+        {
+            model.AccessRequired = [];
+            model.Country = [];
+        }
+
         var context = new ValidationContext<UserViewModel>(model);
         var validationResult = await validator.ValidateAsync(context);
 
@@ -196,6 +203,12 @@ public class UsersController(IUserManagementService userManagementService, IVali
     {
         var mode = string.IsNullOrEmpty(model.Id) ? CreateMode : EditMode;
         ViewBag.Mode = mode;
+
+        if (model.UserRoles.Any(r => r.Name.Equals(OperationsRole, StringComparison.OrdinalIgnoreCase) && !r.IsSelected))
+        {
+            model.AccessRequired = [];
+            model.Country = [];
+        }
 
         var context = new ValidationContext<UserViewModel>(model);
         var validationResult = await validator.ValidateAsync(context);
