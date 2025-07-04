435de8563973a18cfa74e5ee32255e8ef93d8158
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-07-04 15:29:20 +0100

    more changes

--- src/Application/Rsp.IrasPortal.Application/DTOs/CmsQuestionset/CmsQuestionSetResponse.cs
+++ src/Application/Rsp.IrasPortal.Application/DTOs/CmsQuestionset/CmsQuestionSetResponse.cs
@@ -15,6 +15,7 @@ public class SectionModel
     public string SectionName { get; set; }
     public string Id { get; set; }
     public IList<QuestionModel> Questions { get; set; } = new List<QuestionModel>();
+    public IList<ContentComponent> GuidanceComponents { get; set; } = [];
 }
 
 public class QuestionModel
@@ -23,11 +24,12 @@ public class QuestionModel
     public string Id { get; set; }
     public string Key { get; set; }
     public string Label { get; set; }
-    public bool IsMandatory { get; set; }
+    public string Conformance { get; set; }
     public string QuestionFormat { get; set; }
     public string AnswerDataType { get; set; }
     public IList<AnswerModel> Answers { get; set; } = new List<AnswerModel>();
     public IList<RuleModel> ValidationRules { get; set; } = new List<RuleModel>();
+    public IList<ContentComponent> GuidanceComponents { get; set; } = [];
 }
 
 public class AnswerModel
@@ -44,6 +46,7 @@ public class ConditionModel
     public bool Negate { get; set; }
     public string OptionType { get; set; }
     public string Value { get; set; }
+    public string Description { get; set; }
     public IList<AnswerModel> ParentOptions { get; set; }
 }
 
@@ -54,4 +57,42 @@ public class RuleModel
     public string Mode { get; set; }
     public QuestionModel ParentQuestion { get; set; }
     public string QuestionId { get; set; }
+}
+
+public class ContentComponent
+{
+    public string ContentType { get; set; } = null!;
+}
+
+public class DetailsContentComponent : ContentComponent
+{
+    public string Title { get; set; }
+    public string? Value { get; set; }
+}
+
+public class AccordionComponentModel : ContentComponent
+{
+    public IList<AccordionComponentItem> Items { get; set; } = [];
+}
+
+public class AccordionComponentItem
+{
+    public string? Title { get; set; }
+    public string? Value { get; set; }
+}
+
+public class TabsComponentModel : ContentComponent
+{
+    public IList<TabComponentItemModel> Items { get; set; } = [];
+}
+
+public class TabComponentItemModel
+{
+    public string? Title { get; set; }
+    public string? Value { get; set; }
+}
+
+public class BodyTextComponentModel : ContentComponent
+{
+    public string? Value { get; set; }
 }
