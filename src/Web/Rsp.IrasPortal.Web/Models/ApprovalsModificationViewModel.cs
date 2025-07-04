fe0cf38f5452a5019f60d0a20079bb72c94b9bcd
Haris Amin <haris.amin_PA@hra.nhs.uk>
2025-07-04 12:46:57 +0100

    Formatting fixes

---
+++ src/Web/Rsp.IrasPortal.Web/Models/ApprovalsModificationViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Rsp.IrasPortal.Web.Models;
+
+public class ApprovalsModificationViewModel
+{
+    public string ModificationId { get; set; } = null!;
+    public string? ShortProjectTite { get; set; }
+    public string? ModificationType { get; set; }
+    public string? ChiefInvestigator { get; set; }
+    public string? LeadNation { get; set; }
+    public string? SponsorOrganisation { get; set; }
+    public DateOnly? Date { get; set; }
+}
