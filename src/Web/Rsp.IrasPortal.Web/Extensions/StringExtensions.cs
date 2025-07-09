53c63c25caa8a678fa33b0de0db63c2afe4c8440
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-07-09 15:40:04 +0100

    update system admin role casing

---
+++ src/Web/Rsp.IrasPortal.Web/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Rsp.IrasPortal.Web.Extensions;
+
+public static class StringExtensions
+{
+    public static string ToSentenceCase(this string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return input;
+
+        input = input.Trim();
+
+        if (input.Length == 1)
+            return input.ToUpper();
+
+        return char.ToUpper(input[0]) + input.Substring(1).ToLower();
+    }
+}
