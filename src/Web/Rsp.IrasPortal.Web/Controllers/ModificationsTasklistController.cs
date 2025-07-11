1d9d79f5ba335819c0dafdfbf7b41e3e10664c35
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-07-11 16:30:10 +0100

    create modifications tasklist page

---
+++ src/Web/Rsp.IrasPortal.Web/Controllers/ModificationsTasklistController.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Rsp.IrasPortal.Application.DTOs.Requests;
+using Rsp.IrasPortal.Application.Services;
+using Rsp.IrasPortal.Web.Areas.Admin.Models;
+using Rsp.IrasPortal.Web.Models;
+
+namespace Rsp.IrasPortal.Web.Controllers;
+
+[ExcludeFromCodeCoverage]
+[Route("[controller]/[action]", Name = "tasklist:[action]")]
+[Authorize(Policy = "IsUser")]
+public class ModificationsTasklistController(IApplicationsService applicationsService) : Controller
+{
+    public async Task<IActionResult> Index(int pageNumber = 1, int pageSize = 20, IList<string>? selectedModificationIds = null)
+    {
+        var model = new ModificationsTasklistViewModel();
+
+        var modQuery = new ModificationSearchRequest
+        {
+            UnassignedOnly = true,
+        };
+
+        var result = await applicationsService.GetModifications(modQuery, pageNumber, pageSize);
+        model.Modifications = result?.Content?.Modifications?
+                    .Select(dto => new TaskListModificationViewModel
+                    {
+                        Modification = new ModificationsModel
+                        {
+                            ModificationId = dto.ModificationId,
+                            ShortProjectTitle = dto.ShortProjectTitle,
+                            ModificationType = dto.ModificationType,
+                            ChiefInvestigator = dto.ChiefInvestigator,
+                            LeadNation = dto.LeadNation,
+                            SponsorOrganisation = dto.SponsorOrganisation,
+                            CreatedAt = dto.CreatedAt
+                        }
+                    })
+                    .ToList() ?? [];
+
+        foreach (var mod in model.Modifications)
+        {
+            if (selectedModificationIds != null && selectedModificationIds.Contains(mod.Modification.ModificationId))
+            {
+                mod.IsSelected = true;
+            }
+        }
+
+        model.Pagination = new PaginationViewModel(pageNumber, pageSize, result?.Content?.TotalCount ?? 0);
+
+        return View(model);
+    }
+}
