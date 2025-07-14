76440a9ba9d46ff7a0b1ed80d5faeebbd2f7a782
Haris Amin <haris.amin_PA@hra.nhs.uk>
2025-07-14 09:12:01 +0100

    RSP-4090: Sort modificaions after search

--- src/Web/Rsp.IrasPortal.Web/Controllers/ModificationsTasklistController.cs
+++ src/Web/Rsp.IrasPortal.Web/Controllers/ModificationsTasklistController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Rsp.IrasPortal.Application.Constants;
 using Rsp.IrasPortal.Application.DTOs.Requests;
 using Rsp.IrasPortal.Application.Services;
 using Rsp.IrasPortal.Web.Areas.Admin.Models;
@@ -13,16 +14,19 @@ namespace Rsp.IrasPortal.Web.Controllers;
 [Authorize(Policy = "IsUser")]
 public class ModificationsTasklistController(IApplicationsService applicationsService) : Controller
 {
-    public async Task<IActionResult> Index(int pageNumber = 1, int pageSize = 20, IList<string>? selectedModificationIds = null)
+    public async Task<IActionResult> Index(
+        int pageNumber = 1,
+        int pageSize = 20,
+        IList<string>? selectedModificationIds = null,
+        string? sortField = nameof(ModificationsModel.CreatedAt),
+        string? sortDirection = SortDirections.Ascending)
     {
         var model = new ModificationsTasklistViewModel();
 
-        var modQuery = new ModificationSearchRequest
-        {
-            UnassignedOnly = true,
-        };
+        // use this query to add filters
+        var modQuery = new ModificationSearchRequest();
 
-        var result = await applicationsService.GetModifications(modQuery, pageNumber, pageSize);
+        var result = await applicationsService.GetModifications(modQuery, pageNumber, pageSize, sortField, sortDirection);
         model.Modifications = result?.Content?.Modifications?
                     .Select(dto => new TaskListModificationViewModel
                     {
@@ -47,7 +51,11 @@ public class ModificationsTasklistController(IApplicationsService applicationsSe
             }
         }
 
-        model.Pagination = new PaginationViewModel(pageNumber, pageSize, result?.Content?.TotalCount ?? 0);
+        model.Pagination = new PaginationViewModel(pageNumber, pageSize, result?.Content?.TotalCount ?? 0)
+        {
+            SortDirection = sortDirection,
+            SortField = sortField
+        };
 
         return View(model);
     }
