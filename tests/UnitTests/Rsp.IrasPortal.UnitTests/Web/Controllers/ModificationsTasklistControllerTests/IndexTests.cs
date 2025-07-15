8a108ef462491185920c00ed42b2849de1ddc07d
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-07-15 10:55:22 +0100

    add unit tests

---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Controllers/ModificationsTasklistControllerTests/IndexTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rsp.IrasPortal.Web.Controllers;
+
+namespace Rsp.IrasPortal.UnitTests.Web.Controllers.ModificationsTasklistControllerTests;
+
+public class IndexTests : TestServiceBase<ModificationsTasklistController>
+{
+    [Fact]
+    public async Task Welcome_ReturnsViewResult_WithIndexViewName()
+    {
+        // Act
+        var result = await Sut.Index(1, 20, null, "CreatedAt", "asc");
+
+        // Assert
+        result.ShouldBeOfType<ViewResult>();
+    }
+}
