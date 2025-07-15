a1caf51f5926d9d84d2ffbb974e67c9ed5cc335b
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-07-15 16:27:05 +0100

    RSP-3556 Implement advanced filters on the Manage users page

---
+++ src/Web/Rsp.IrasPortal.Web/Validators/UserSearchModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Rsp.IrasPortal.Web.Models;
+
+namespace Rsp.IrasPortal.Web.Validators;
+
+public class UserSearchModelValidator : AbstractValidator<UserSearchModel>
+{
+    public UserSearchModelValidator()
+    {
+        // Only run this rule if both dates are entered
+        When(x => x.FromDate.HasValue && x.ToDate.HasValue, () =>
+        {
+            RuleFor(x => x)
+                .Must(x => x.ToDate >= x.FromDate)
+                .WithMessage("'Search to' date must be after 'Search from' date")
+                .WithName("Search.ToDate");
+        });
+    }
+}
