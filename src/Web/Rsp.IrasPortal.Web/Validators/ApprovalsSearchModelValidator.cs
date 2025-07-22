7d9e406b2ca7ed36823589b7417664fa99b879f7
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-07-22 18:28:52 +0100

    RSP-4167 Update the UI for Advanced filters

--- src/Web/Rsp.IrasPortal.Web/Validators/ApprovalsSearchModelValidator.cs
+++ src/Web/Rsp.IrasPortal.Web/Validators/ApprovalsSearchModelValidator.cs
@@ -7,13 +7,41 @@ public class ApprovalsSearchModelValidator : AbstractValidator<ApprovalsSearchMo
 {
     public ApprovalsSearchModelValidator()
     {
-        // Only run this rule if both dates are entered
+        // Validate "From" date is valid if any part entered
+        When(AnyFromDateFieldEntered, () =>
+        {
+            RuleFor(x => x.FromDate)
+                .NotNull()
+                .WithMessage("'Search from' date must be in the correct format")
+                .WithName("FromDate");
+        });
+
+        // Validate "To" date is valid if any part entered
+        When(AnyToDateFieldEntered, () =>
+        {
+            RuleFor(x => x.ToDate)
+                .NotNull()
+                .WithMessage("'Search to' date must be in the correct format")
+                .WithName("ToDate");
+        });
+
+        // Validate date order only if both valid
         When(x => x.FromDate.HasValue && x.ToDate.HasValue, () =>
         {
             RuleFor(x => x)
                 .Must(x => x.ToDate >= x.FromDate)
-                .WithMessage("'Search to' date must be after 'Search from' date")
-                .WithName("Search.ToDate");
+                .WithMessage("The date you’ve selected is before the search above")
+                .WithName("ToDate");
         });
     }
+
+    private bool AnyFromDateFieldEntered(ApprovalsSearchModel model) =>
+        !string.IsNullOrWhiteSpace(model.FromDay) ||
+        !string.IsNullOrWhiteSpace(model.FromMonth) ||
+        !string.IsNullOrWhiteSpace(model.FromYear);
+
+    private bool AnyToDateFieldEntered(ApprovalsSearchModel model) =>
+        !string.IsNullOrWhiteSpace(model.ToDay) ||
+        !string.IsNullOrWhiteSpace(model.ToMonth) ||
+        !string.IsNullOrWhiteSpace(model.ToYear);
 }
