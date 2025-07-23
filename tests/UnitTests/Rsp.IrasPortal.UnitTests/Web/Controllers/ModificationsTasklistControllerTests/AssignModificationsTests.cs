1179ee3ecaa6c5364243b7aef4cd4881bfdbf6c1
MateuszBoreckiHRA <mateusz.borecki_pa@hra.nhs.uk>
2025-07-23 12:46:49 +0200

    Added error for modification to assign not selected

---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Controllers/ModificationsTasklistControllerTests/AssignModificationsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Rsp.IrasPortal.Application.Constants;
+using Rsp.IrasPortal.Web.Controllers;
+using Rsp.IrasPortal.Web.Extensions;
+
+namespace Rsp.IrasPortal.UnitTests.Web.Controllers.ModificationsTasklistControllerTests;
+
+public class AssignModificationsTests : TestServiceBase<ModificationsTasklistController>
+{
+    [Fact]
+    public async Task AssignModifications_WhenNoIdsSelected_AddsModelErrorAndRedirects()
+    {
+        // Arrange
+        var tempData = new TempDataDictionary(new DefaultHttpContext(), Mock.Of<ITempDataProvider>());
+        Sut.TempData = tempData;
+
+        // Act
+        var result = await Sut.AssignModifications(new List<string>());
+
+        // Assert
+        Sut.ModelState.ContainsKey("ModificationToAssignNotSelected").ShouldBeTrue();
+        tempData.ContainsKey(TempDataKeys.ModelState).ShouldBeTrue();
+
+        var redirect = result.ShouldBeOfType<RedirectToActionResult>();
+        redirect.ActionName.ShouldBe(nameof(Sut.Index));
+    }
+}
