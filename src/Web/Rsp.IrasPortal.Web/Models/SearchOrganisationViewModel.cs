f566f93c2848893927258524e0fa5cf2b7a18591
MateuszBoreckiHRA <mateusz.borecki_pa@hra.nhs.uk>
2025-07-24 15:26:16 +0200

    Started modification of SearchOrganisation view
    - added tab component
    - added advanced filters for Organisations (not logic, just view for now)
    - added detail element - [can't find your organisation?]

--- src/Web/Rsp.IrasPortal.Web/Models/SearchOrganisationViewModel.cs
+++ src/Web/Rsp.IrasPortal.Web/Models/SearchOrganisationViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Rsp.IrasPortal.Web.Models;
+﻿using Rsp.IrasPortal.Web.Areas.Admin.Models;
+
+namespace Rsp.IrasPortal.Web.Models;
 
 /// <summary>
 /// View model for handling organisation search input as part of the project modification journey.
@@ -10,4 +12,12 @@ public class SearchOrganisationViewModel : BaseProjectModificationViewModel
     /// Gets or sets the search term entered by the user to find an organisation.
     /// </summary>
     public string? SearchTerm { get; set; }
+
+    // TEMP - FOR CHANGE TO ORGANIZATIONS
+    public List<string> SelectedModificationIds { get; set; } = [];
+
+    public UserSearchModel Search { get; set; } = new();
+
+    public IEnumerable<UserViewModel> Organisations { get; set; } = [];
+    public PaginationViewModel? Pagination { get; set; }
 }
