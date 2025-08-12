58667192066f88a42de1b575b2bb48a225387218
Vinay Lingappa <vinay.lingappa_PA@hra.nhs.uk>
2025-08-12 18:24:49 +0100

    - unit tests for CmsQuestionsetService

---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Services/CmsQuestionsetServiceTests/GetInitialModificationQuestionsTests.cs
@@ -0,0 +1,45 @@
+﻿using Rsp.IrasPortal.Application.DTOs.CmsQuestionset.Modifications;
+using Rsp.IrasPortal.Application.ServiceClients;
+using Rsp.IrasPortal.Services;
+
+namespace Rsp.IrasPortal.UnitTests.Services.CmsQuestionsetServiceTests;
+
+public class GetInitialModificationQuestionsTests : TestServiceBase<CmsQuestionsetService>
+{
+    [Fact]
+    public async Task GetInitialModificationQuestions_ShouldReturnSuccess_WhenApiReturnsOk()
+    {
+        // Arrange
+        var apiResponse = new ApiResponse<StartingQuestionsModel>
+        (
+            new HttpResponseMessage(HttpStatusCode.OK),
+            new StartingQuestionsModel(),
+            new()
+        );
+
+        Mocker
+            .GetMock<ICmsQuestionSetServiceClient>()
+            .Setup(c => c.GetInitialModificationQuestions())
+            .ReturnsAsync(apiResponse);
+
+        // Act
+        var result = await Sut.GetInitialModificationQuestions();
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GetInitialModificationQuestions_ShouldReturnError_WhenApiFails()
+    {
+        // Arrange
+        Mocker
+            .GetMock<ICmsQuestionSetServiceClient>()
+            .Setup(c => c.GetInitialModificationQuestions())
+            .ThrowsAsync(new Exception("API failure"));
+
+        // Act & Assert
+        await Should.ThrowAsync<Exception>(() => Sut.GetInitialModificationQuestions());
+    }
+}
---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Services/CmsQuestionsetServiceTests/GetModificationsJourneyTests.cs
@@ -0,0 +1,49 @@
+﻿using Rsp.IrasPortal.Application.DTOs.CmsQuestionset;
+using Rsp.IrasPortal.Application.ServiceClients;
+using Rsp.IrasPortal.Services;
+
+namespace Rsp.IrasPortal.UnitTests.Services.CmsQuestionsetServiceTests;
+
+public class GetModificationsJourneyTests : TestServiceBase<CmsQuestionsetService>
+{
+    private readonly Mock<ICmsQuestionSetServiceClient> _questionSetServiceClient;
+
+    public GetModificationsJourneyTests()
+    {
+        _questionSetServiceClient = Mocker.GetMock<ICmsQuestionSetServiceClient>();
+    }
+
+    [Theory, AutoData]
+    public async Task GetModificationsJourney_ShouldReturnSuccess_WhenApiReturnsOk(
+        string specificChangeId)
+    {
+        // Arrange
+        var apiResponse = new ApiResponse<CmsQuestionSetResponse>
+        (
+            new HttpResponseMessage(HttpStatusCode.OK),
+            new CmsQuestionSetResponse(),
+            new()
+        );
+
+        _questionSetServiceClient
+            .Setup(c => c.GetModificationsJourney(specificChangeId))
+            .ReturnsAsync(apiResponse);
+
+        var result = await Sut.GetModificationsJourney(specificChangeId);
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.IsSuccessStatusCode.ShouldBeTrue();
+        result.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+
+    [Theory, AutoData]
+    public async Task GetModificationsJourney_ShouldThrow_WhenApiFails(string specificChangeId)
+    {
+        _questionSetServiceClient
+            .Setup(c => c.GetModificationsJourney(specificChangeId))
+            .ThrowsAsync(new Exception("Bad request"));
+
+        await Should.ThrowAsync<Exception>(() => Sut.GetModificationsJourney(specificChangeId));
+    }
+}
