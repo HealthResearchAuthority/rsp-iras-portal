62830a7de59b9dc3bab04c29d4d1689e0a3fd986
Vinay Lingappa <vinay.lingappa_PA@hra.nhs.uk>
2025-08-12 18:24:49 +0100

    - unit tests for CmsQuestionsetService

---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Services/CmsQuestionsetServiceTests/GetQuestionSectionsTests.cs
@@ -0,0 +1,49 @@
+﻿using Rsp.IrasPortal.Application.DTOs;
+using Rsp.IrasPortal.Application.ServiceClients;
+using Rsp.IrasPortal.Services;
+
+namespace Rsp.IrasPortal.UnitTests.Services.CmsQuestionsetServiceTests;
+
+public class GetQuestionSectionsTests : TestServiceBase<CmsQuestionsetService>
+{
+    private readonly Mock<ICmsQuestionSetServiceClient> _questionSetServiceClient;
+
+    public GetQuestionSectionsTests()
+    {
+        _questionSetServiceClient = Mocker.GetMock<ICmsQuestionSetServiceClient>();
+    }
+
+    [Theory, AutoData]
+    public async Task GetQuestionSections_ShouldReturnSuccess_WhenApiReturnsOk(
+        IEnumerable<QuestionSectionsResponse> expectedResponse)
+    {
+        // Arrange
+        var apiResponse = new ApiResponse<IEnumerable<QuestionSectionsResponse>>
+        (
+            new HttpResponseMessage(HttpStatusCode.OK),
+            new List<QuestionSectionsResponse>(),
+            new()
+        );
+
+        _questionSetServiceClient
+            .Setup(c => c.GetQuestionSections())
+            .ReturnsAsync(apiResponse);
+
+        var result = await Sut.GetQuestionSections();
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.IsSuccessStatusCode.ShouldBeTrue();
+        result.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GetQuestionSections_ShouldThrow_WhenApiFails()
+    {
+        _questionSetServiceClient
+            .Setup(c => c.GetQuestionSections())
+            .ThrowsAsync(new Exception("Timeout"));
+
+        await Should.ThrowAsync<Exception>(() => Sut.GetQuestionSections());
+    }
+}
