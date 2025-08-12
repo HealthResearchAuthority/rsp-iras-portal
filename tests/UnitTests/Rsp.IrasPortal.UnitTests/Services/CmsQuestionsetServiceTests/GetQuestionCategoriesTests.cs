b96d5beaea3660b757948faa9fe8b30d4776d6b2
Vinay Lingappa <vinay.lingappa_PA@hra.nhs.uk>
2025-08-12 18:24:49 +0100

    - unit tests for CmsQuestionsetService

---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Services/CmsQuestionsetServiceTests/GetQuestionCategoriesTests.cs
@@ -0,0 +1,50 @@
+﻿using Rsp.IrasPortal.Application.DTOs;
+using Rsp.IrasPortal.Application.ServiceClients;
+using Rsp.IrasPortal.Services;
+
+namespace Rsp.IrasPortal.UnitTests.Services.CmsQuestionsetServiceTests;
+
+public class GetQuestionCategoriesTests : TestServiceBase<CmsQuestionsetService>
+{
+    private readonly Mock<ICmsQuestionSetServiceClient> _questionSetServiceClient;
+
+    public GetQuestionCategoriesTests()
+    {
+        _questionSetServiceClient = Mocker.GetMock<ICmsQuestionSetServiceClient>();
+    }
+
+    [Theory, AutoData]
+    public async Task GetQuestionCategories_ShouldReturnSuccess_WhenApiReturnsOk(
+        IEnumerable<CategoryDto> expectedResponse)
+    {
+        // Arrange
+        var apiResponse = new ApiResponse<IEnumerable<CategoryDto>>
+        (
+            new HttpResponseMessage(HttpStatusCode.OK),
+            new List<CategoryDto>(),
+            new()
+        );
+
+        _questionSetServiceClient
+            .Setup(c => c.GetQuestionCategories())
+            .ReturnsAsync(apiResponse);
+
+        // Act
+        var result = await Sut.GetQuestionCategories();
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.IsSuccessStatusCode.ShouldBeTrue();
+        result.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GetQuestionCategories_ShouldThrow_WhenApiFails()
+    {
+        _questionSetServiceClient
+            .Setup(c => c.GetQuestionCategories())
+            .ThrowsAsync(new Exception("Error fetching categories"));
+
+        await Should.ThrowAsync<Exception>(() => Sut.GetQuestionCategories());
+    }
+}
