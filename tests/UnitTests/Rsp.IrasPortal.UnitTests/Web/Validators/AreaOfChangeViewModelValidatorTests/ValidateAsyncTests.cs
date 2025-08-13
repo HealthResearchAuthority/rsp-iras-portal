f6c3b71c5be316831e29266287c62a210ed87964
Vinay Lingappa <vinay.lingappa_PA@hra.nhs.uk>
2025-08-13 14:58:45 +0100

    - AreaOfChangeViewModelValidator tests

---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Validators/AreaOfChangeViewModelValidatorTests/ValidateAsyncTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation.TestHelper;
+using Rsp.IrasPortal.Web.Models;
+using Rsp.IrasPortal.Web.Validators;
+
+namespace Rsp.IrasPortal.UnitTests.Web.Validators.AreaOfChangeViewModelValidatorTests;
+
+public class ValidateAsyncTests : TestServiceBase<AreaOfChangeViewModelValidator>
+{
+    [Fact]
+    public async Task Should_Fail_When_AreaOfChangeIdIsNull()
+    {
+        // Arrange
+        var model = new AreaOfChangeViewModel
+        {
+            AreaOfChangeId = null,
+            SpecificChangeId = Guid.NewGuid().ToString(),
+        };
+
+        // Act
+        var result = await Sut.TestValidateAsync(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.AreaOfChangeId)
+              .WithErrorMessage("Select area of change");
+    }
+
+    [Fact]
+    public async Task Should_Fail_When_SpecificAreaOfChangeIdIsNull()
+    {
+        // Arrange
+        var model = new AreaOfChangeViewModel
+        {
+            AreaOfChangeId = Guid.NewGuid().ToString(),
+            SpecificChangeId = null
+        };
+
+        // Act
+        var result = await Sut.TestValidateAsync(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.SpecificChangeId)
+              .WithErrorMessage("Select specific change");
+    }
+}
