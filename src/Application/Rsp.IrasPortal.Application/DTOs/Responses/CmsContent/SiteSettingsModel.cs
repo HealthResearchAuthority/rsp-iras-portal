180e3dbda8c1ef9140ca88948f2d718240aa8bf6
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-08-14 13:38:32 +0100

    resolve conflicts

---
+++ src/Application/Rsp.IrasPortal.Application/DTOs/Responses/CmsContent/SiteSettingsModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Rsp.IrasPortal.Application.DTOs.Responses.CmsContent;
+
+public class SiteSettingsModel
+{
+    public IList<LinkModel>? FooterLinks { get; set; }
+    public string? LoginLandingPageContent { get; set; }
+}
