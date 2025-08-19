18fdd1663718b05790dfb45ffd679581be6c0504
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-08-19 09:32:10 +0100

    handle static content

---
+++ src/Web/Rsp.IrasPortal.Web/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Rsp.IrasPortal.Web.Extensions;
+
+public static class DictionaryExtensions
+{
+    /// <summary>
+    /// Gets the value for the given key, or returns a default value if not found.
+    /// </summary>
+    public static string GetValueOrDefault(this IDictionary<string, string?> dict, string key, string defaultValue = "")
+    {
+        if (dict == null)
+            throw new ArgumentNullException(nameof(dict));
+
+        if (key == null)
+            throw new ArgumentNullException(nameof(key));
+
+        return dict.TryGetValue(key, out var value) ? value : defaultValue;
+    }
+}
