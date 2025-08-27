17bb415c2093c46447e95c81071a0b749c06800a
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-08-27 10:51:33 +0100

    add additional unit tests

---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Services/UserManagementServiceTests/UpdateUserIdentityProviderIdTests.cs
@@ -0,0 +1,47 @@
+﻿using Rsp.IrasPortal.Application.Responses;
+using Rsp.IrasPortal.Application.ServiceClients;
+using Rsp.IrasPortal.Domain.Identity;
+using Rsp.IrasPortal.Services;
+
+namespace Rsp.IrasPortal.UnitTests.Services.UserManagementServiceTests;
+
+public class UpdateUserIdentityProviderIdTests : TestServiceBase<UserManagementService>
+{
+    [Theory, AutoData]
+    public async Task Throw_Error_When_Parameter_Is_Null(User existingUser)
+    {
+        // execute
+        var result = await Sut.UpdateUserIdentityProviderId(existingUser, string.Empty);
+
+        // verify
+        result.ShouldBeOfType<ServiceResponse>();
+        result.IsSuccessStatusCode.ShouldBeFalse();
+        result.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+
+    [Theory, AutoData]
+    public async Task Execute_When_Parameter_Is_Fine(User existingUser, string identityProviderId)
+    {
+        // setup
+        // Arrange
+        var apiResponse = Mock.Of<IApiResponse>
+        (
+            apiResponse =>
+                apiResponse.IsSuccessStatusCode &&
+                apiResponse.StatusCode == HttpStatusCode.OK
+        );
+
+        var service = Mocker.GetMock<IUserManagementServiceClient>();
+        service
+            .Setup(c => c.UpdateUser(It.IsAny<string>(), It.IsAny<User>()))
+            .ReturnsAsync(apiResponse);
+
+        // execute
+        var result = await Sut.UpdateUserIdentityProviderId(existingUser, identityProviderId);
+
+        // verify
+        result.ShouldBeOfType<ServiceResponse>();
+        result.IsSuccessStatusCode.ShouldBeTrue();
+        result.StatusCode.ShouldBe(HttpStatusCode.OK);
+    }
+}
