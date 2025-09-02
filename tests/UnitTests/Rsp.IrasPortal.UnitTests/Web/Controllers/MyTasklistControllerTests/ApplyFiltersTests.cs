68c52a3e2d60f4780e28e9904b04383db3c9da37
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-09-02 14:53:32 +0100

    Updated unit tests

---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Controllers/MyTasklistControllerTests/ApplyFiltersTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Rsp.IrasPortal.Web.Controllers;
+using Rsp.IrasPortal.Web.Models;
+
+namespace Rsp.IrasPortal.UnitTests.Web.Controllers.MyTasklistControllerTests;
+
+public class ApplyFiltersTests : TestServiceBase<MyTasklistController>
+{
+    [Fact]
+    public async Task ApplyFilters_ShouldAddAllValidationErrors_WhenModelIsInvalid()
+    {
+        // Arrange
+        var searchModel = new ApprovalsSearchModel { FromYear = "not-a-year" };
+        var viewModel = new MyTasklistViewModel() { Search = searchModel };
+
+        var validationResult = new ValidationResult(
+        [
+            new ValidationFailure("FromYear", "Must be numeric")
+        ]);
+
+        SetupValidatorResult(validationResult);
+
+        // Act
+        var result = await Sut.ApplyFilters(viewModel);
+
+        // Assert
+        var viewResult = result.ShouldBeOfType<ViewResult>();
+        viewResult.ViewName.ShouldBe("Index");
+
+        Sut.ModelState.IsValid.ShouldBeFalse();
+        Sut.ModelState["FromYear"]!.Errors.Single().ErrorMessage.ShouldBe("Must be numeric");
+    }
+
+    private void SetupValidatorResult(ValidationResult result)
+    {
+        var mockValidator = Mocker.GetMock<IValidator<ApprovalsSearchModel>>();
+        mockValidator
+            .Setup(v => v.ValidateAsync(It.IsAny<ApprovalsSearchModel>(), default))
+            .ReturnsAsync(result);
+    }
+}
---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Controllers/MyTasklistControllerTests/ClearFiltersTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Rsp.IrasPortal.Application.Constants;
+using Rsp.IrasPortal.Web.Controllers;
+using Rsp.IrasPortal.Web.Models;
+
+namespace Rsp.IrasPortal.UnitTests.Web.Controllers.MyTasklistControllerTests;
+
+public class ClearFiltersTests : TestServiceBase<MyTasklistController>
+{
+    private readonly DefaultHttpContext _http;
+
+    public ClearFiltersTests()
+    {
+        _http = new DefaultHttpContext
+        {
+            Session = new InMemorySession()
+        };
+
+        Sut.ControllerContext = new ControllerContext
+        {
+            HttpContext = _http
+        };
+    }
+
+    [Fact]
+    public void ClearFilters_ShouldRedirectToSearch()
+    {
+        // Act
+        var result = Sut.ClearFilters();
+
+        // Assert
+        var redirectResult = result.ShouldBeOfType<RedirectToActionResult>();
+        redirectResult.ActionName.ShouldBe(nameof(Sut.Index));
+    }
+
+    [Fact]
+    public void ClearFilters_ShouldRetainOnlyIrasIdAndRedirect()
+    {
+        // Arrange
+        var originalSearch = new ApprovalsSearchModel
+        {
+            IrasId = "IRAS123",
+            ShortProjectTitle = "TestOrg",
+        };
+
+        _http.Session.SetString(SessionKeys.MyTasklist, JsonSerializer.Serialize(originalSearch));
+
+        // Act
+        var result = Sut.ClearFilters();
+
+        // Assert
+        var redirectResult = result.ShouldBeOfType<RedirectToActionResult>();
+        redirectResult.ActionName.ShouldBe(nameof(Sut.Index));
+
+        var updatedJson = _http.Session.GetString(SessionKeys.MyTasklist);
+        updatedJson.ShouldNotBeNull();
+
+        var updatedSearch = JsonSerializer.Deserialize<ApprovalsSearchModel>(updatedJson!);
+        updatedSearch.ShouldNotBeNull();
+        updatedSearch!.IrasId.ShouldBe("IRAS123");
+        updatedSearch.ShortProjectTitle.ShouldBeNull();
+    }
+}
+
+
+
+
