a8d70b2ba7a33d7493bcec85564be977aeece216
MateuszBoreckiHRA <mateusz.borecki_pa@hra.nhs.uk>
2025-09-08 14:18:21 +0200

    Implemented Modification Details page

---
+++ src/Web/Rsp.IrasPortal.Web/Models/ModificationChangeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Rsp.IrasPortal.Web.Models;
+
+public class ModificationChangeModel
+{
+    public Guid ModificationChangeId { get; set; }
+    public string? ModificationType { get; set; }
+    public string? Category { get; set; }
+    public string? ReviewType { get; set; }
+    public string AreaOfChangeName { get; set; } = null!;
+    public string SpecificChangeName { get; set; } = null!;
+    public string? SpecificChangeAnswer { get; set; }
+    public string ChangeStatus { get; set; } = null!;
+}
