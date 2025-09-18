284bd2ae850610c00797c828cd56203993cff34f
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-09-18 13:37:05 +0100

    RSP-5047 RSP-5048 RSP-5049 RSP-5050 My Research Updates

--- src/Application/Rsp.IrasPortal.Application/Constants/ProjectOverview.cs
+++ src/Application/Rsp.IrasPortal.Application/Constants/ProjectOverview.cs
@@ -4,7 +4,7 @@ public static class ProjectOverview
 {
     public const string ProjectDetails = "Project details";
     public const string ProjectDocuments = "Project documents";
-    public const string KeyProjectRoles = "Key project roles";
+    public const string ProjectTeam = "Project team";
     public const string ResearchLocations = "Research locations";
     public const string PostApproval = "Post approval";
     public const string ProjectHistory = "Project history";
