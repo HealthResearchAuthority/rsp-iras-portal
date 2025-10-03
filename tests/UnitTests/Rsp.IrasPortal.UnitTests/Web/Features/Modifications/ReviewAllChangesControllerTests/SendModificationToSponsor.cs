a0ef600c11518bcf2733b84284ece53bfd606f13
vinaylingappa-HRA <vinay.lingappa_PA@hra.nhs.uk>
2025-10-03 08:41:32 +0100

    Submit to regulator (#276)
    ## What was the purpose of this ticket?
    Submit to regulator
    
    ## Jira Ref
    [RSP-4863](https://nihr.atlassian.net/browse/RSP-4863)
    
    ## Type of Change
    
    Put [X] inside the [] to make the box ticked
    
    - [x] New feature
    - [] Refactoring
    - [] Bug-fix
    - [] DevOps
    - [] Testing
    
    ## Solution
    
    What work was completed to cover off the story?
    
    - List out changes made to the repo
    
    ## Checklist
    
    - [ ] Your code builds clean without any  warnings
    - [ ] You have added unit tests
    - [ ] All the added unit tests add value i.e. assert the right thing?
    - [ ] You are using the correct naming conventions
    - [ ] You have fixed all the suggestions made by .editorconfig and/or
    Roslynator
    - [ ] There are no dead code and no "TODO" comments left
    - [ ] Please make sure that using statements are sorted with using
    System* statements at the top. You can use CTRL + K, CTRL + D command to
    format the file, that will respect the .editorconfig, or use the Visual
    Studio extension called [CodeMaid](http://www.codemaid.net/) to do that
    for you automatically on save.

---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Features/Modifications/ReviewAllChangesControllerTests/SendModificationToSponsor.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Rsp.IrasPortal.Application.Constants;
+using Rsp.IrasPortal.Application.Responses;
+using Rsp.IrasPortal.Application.Services;
+using Rsp.IrasPortal.Web.Features.Modifications;
+
+namespace Rsp.IrasPortal.UnitTests.Web.Features.Modifications.ReviewAllChangesControllerTests;
+
+public class SendModificationToSponsor : TestServiceBase<ReviewAllChangesController>
+{
+    [Theory, AutoData]
+    public async Task SendModificationToSponsor_Should_Return_View_When_Success(
+        string projectRecordId,
+        Guid projectModificationId)
+    {
+        // Arrange
+        var http = new DefaultHttpContext();
+        Sut.ControllerContext = new ControllerContext { HttpContext = http };
+        Sut.TempData = new TempDataDictionary(http, Mock.Of<ITempDataProvider>());
+
+        var response = new ServiceResponse
+        {
+            StatusCode = HttpStatusCode.OK
+        };
+
+        Mocker.GetMock<IProjectModificationsService>()
+            .Setup(s => s.UpdateModificationStatus(projectModificationId, ModificationStatus.InSponsorReview))
+            .ReturnsAsync(response);
+
+        // Act
+        var result = await Sut.SendModificationToSponsor(projectRecordId, projectModificationId);
+
+        // Assert
+        var viewResult = result.ShouldBeOfType<ViewResult>();
+        viewResult.ViewName.ShouldBe("ModificationSentToSponsor");
+
+        // Verify
+        Mocker.GetMock<IProjectModificationsService>()
+            .Verify(s => s.UpdateModificationStatus(projectModificationId, ModificationStatus.InSponsorReview), Times.Once);
+    }
+
+    [Theory, AutoData]
+    public async Task SubmitToRegulator_Should_Redirect_When_Success(
+        string projectRecordId,
+        Guid projectModificationId)
+    {
+        // Arrange
+        var http = new DefaultHttpContext();
+        Sut.ControllerContext = new ControllerContext { HttpContext = http };
+        Sut.TempData = new TempDataDictionary(http, Mock.Of<ITempDataProvider>());
+
+        var response = new ServiceResponse
+        {
+            StatusCode = HttpStatusCode.OK
+        };
+
+        Mocker.GetMock<IProjectModificationsService>()
+            .Setup(s => s.UpdateModificationStatus(projectModificationId, ModificationStatus.Approved))
+            .ReturnsAsync(response);
+
+        // Act
+        var result = await Sut.SubmitToRegulator(projectRecordId, projectModificationId);
+
+        // Assert
+        var redirectResult = result.ShouldBeOfType<RedirectToRouteResult>();
+        redirectResult.RouteName.ShouldBe("pov:projectdetails");
+        redirectResult.RouteValues.ShouldContainKeyAndValue("projectRecordId", projectRecordId);
+
+        // Verify
+        Mocker.GetMock<IProjectModificationsService>()
+            .Verify(s => s.UpdateModificationStatus(projectModificationId, ModificationStatus.Approved), Times.Once);
+    }
+
+    [Theory, AutoData]
+    public async Task SendModificationToSponsor_Should_Return_StatusCode_When_Failure(
+        string projectRecordId,
+        Guid projectModificationId)
+    {
+        // Arrange
+        var http = new DefaultHttpContext();
+        Sut.ControllerContext = new ControllerContext { HttpContext = http };
+        Sut.TempData = new TempDataDictionary(http, Mock.Of<ITempDataProvider>());
+
+        var response = new ServiceResponse
+        {
+            StatusCode = HttpStatusCode.InternalServerError
+        };
+
+        Mocker.GetMock<IProjectModificationsService>()
+            .Setup(s => s.UpdateModificationStatus(projectModificationId, ModificationStatus.InSponsorReview))
+            .ReturnsAsync(response);
+
+        // Act
+        var result = await Sut.SendModificationToSponsor(projectRecordId, projectModificationId);
+
+        // Assert
+        var status = result.ShouldBeOfType<StatusCodeResult>();
+        status.StatusCode.ShouldBe(StatusCodes.Status500InternalServerError);
+    }
+
+    [Theory, AutoData]
+    public async Task SubmitToRegulator_Should_Return_StatusCode_When_Failure(
+        string projectRecordId,
+        Guid projectModificationId)
+    {
+        // Arrange
+        var http = new DefaultHttpContext();
+        Sut.ControllerContext = new ControllerContext { HttpContext = http };
+        Sut.TempData = new TempDataDictionary(http, Mock.Of<ITempDataProvider>());
+
+        var response = new ServiceResponse
+        {
+            StatusCode = HttpStatusCode.BadRequest
+        };
+
+        Mocker.GetMock<IProjectModificationsService>()
+            .Setup(s => s.UpdateModificationStatus(projectModificationId, ModificationStatus.Approved))
+            .ReturnsAsync(response);
+
+        // Act
+        var result = await Sut.SubmitToRegulator(projectRecordId, projectModificationId);
+
+        // Assert
+        var status = result.ShouldBeOfType<StatusCodeResult>();
+        status.StatusCode.ShouldBe(StatusCodes.Status400BadRequest);
+    }
+}
