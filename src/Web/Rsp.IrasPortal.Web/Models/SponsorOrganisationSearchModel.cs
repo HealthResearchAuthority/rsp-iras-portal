064a462ab54a718e5196e2454ee26ebf9572010e
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-10-08 10:19:41 +0100

    Reduce duplicated lines

--- src/Web/Rsp.IrasPortal.Web/Models/SponsorOrganisationSearchModel.cs
+++ src/Web/Rsp.IrasPortal.Web/Models/SponsorOrganisationSearchModel.cs
@@ -3,34 +3,6 @@
 namespace Rsp.IrasPortal.Web.Models;
 
 [ExcludeFromCodeCoverage]
-public class SponsorOrganisationSearchModel
+public class SponsorOrganisationSearchModel : SearchModel
 {
-    public string? SearchQuery { get; set; }
-    public List<string> Country { get; set; } = [];
-    public bool? Status { get; set; }
-
-    public Dictionary<string, string> Filters
-    {
-        get
-        {
-            var filters = new Dictionary<string, string>();
-
-            if (Country?.Count != 0 && Country != null)
-            {
-                filters.Add("Country", string.Join(", ", Country));
-            }
-
-            switch (Status)
-            {
-                case true:
-                    filters.Add("Status", "Active");
-                    break;
-                case false:
-                    filters.Add("Status", "Disabled");
-                    break;
-            }
-
-            return filters;
-        }
-    }
 }
