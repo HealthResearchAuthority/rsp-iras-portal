39a730cfe5a8476f9d2a2fd0e55169de642d2768
MateuszBoreckiHRA <mateusz.borecki_pa@hra.nhs.uk>
2025-10-14 16:14:51 +0200

    Added unit tests

---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Controllers/ApplicationControllerTests/ApplyFiltersTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Rsp.IrasPortal.Web.Controllers;
+using Rsp.IrasPortal.Web.Models;
+
+namespace Rsp.IrasPortal.UnitTests.Web.Controllers.ApplicationControllerTests;
+
+public class ApplyFiltersTests : TestServiceBase<ApplicationController>
+{
+    [Fact]
+    public async Task ApplyFilters_ShouldAddAllValidationErrors_WhenModelIsInvalid()
+    {
+        // Arrange
+        var searchModel = new ApplicationSearchModel { FromYear = "not-a-year" };
+        var viewModel = new ApplicationsViewModel { Search = searchModel };
+
+        var validationResult = new ValidationResult(
+        [
+            new ValidationFailure("FromYear", "Must be numeric")
+        ]);
+
+        SetupValidatorResult(validationResult);
+
+        // Act
+        var result = await Sut.ApplyFilters(viewModel);
+
+        // Assert
+        var viewResult = result.ShouldBeOfType<ViewResult>();
+        viewResult.ViewName.ShouldBe("Index");
+
+        Sut.ModelState.IsValid.ShouldBeFalse();
+        Sut.ModelState["FromYear"]!.Errors.Single().ErrorMessage.ShouldBe("Must be numeric");
+    }
+
+    private void SetupValidatorResult(ValidationResult result)
+    {
+        var mockValidator = Mocker.GetMock<IValidator<ApplicationSearchModel>>();
+        mockValidator
+            .Setup(v => v.ValidateAsync(It.IsAny<ApplicationSearchModel>(), default))
+            .ReturnsAsync(result);
+    }
+}
