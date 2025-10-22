74cd5a9e82f99e3eec529bfd054232dbac2936dd
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-10-22 10:48:51 +0100

    add cookie banner and cookies settings page

---
+++ src/Web/Rsp.IrasPortal.Web/Features/CookiePolicy/ViewComponents/CookieBannerViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rsp.IrasPortal.Application.Constants;
+
+namespace Rsp.IrasPortal.Web.Features.CookiePolicy.Components;
+
+public class CookieBannerViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        bool hasConsent = Request.Cookies.ContainsKey(CookieConsentNames.EssentialCookies);
+
+        if (hasConsent)
+            return Content(string.Empty); // Don't show banner again
+
+        return View("~/Features/CookiePolicy/Views/CookieBanner.cshtml");
+    }
+}
