23b2b3c928faeaaaadf640e6e01bf9f754890f03
Vinay Lingappa <vinay.lingappa_PA@hra.nhs.uk>
2025-10-23 17:38:32 +0100

    - fixing code coverage issues

--- tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Features/Modifications/Documents/DownloadDocumentTests.cs
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Features/Modifications/Documents/DownloadDocumentTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Rsp.IrasPortal.Application.Responses;
 using Rsp.IrasPortal.Application.Services;
 using Rsp.IrasPortal.Web.Features.Modifications.Documents.Controllers;
@@ -46,5 +47,80 @@ public class DownloadDocumentTests : TestServiceBase<DocumentsController>
                 path,
                 fileName),
             Times.Once);
+    }
+
+    [Fact]
+    public async Task DownloadDocument_WhenFileNotFound_ReturnsNotFound()
+    {
+        // Arrange
+        var serviceResponse = new ServiceResponse<IActionResult>()
+            .WithContent(new NotFoundObjectResult("File not found"), HttpStatusCode.NotFound);
+
+        Mocker.GetMock<IBlobStorageService>()
+            .Setup(s => s.DownloadFileToHttpResponseAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+            .ReturnsAsync(serviceResponse);
+
+        // Act
+        var result = await Sut.DownloadDocument("invalid/path", "missing.txt");
+
+        // Assert
+        result.ShouldBeOfType<NotFoundObjectResult>();
+    }
+
+    [Fact]
+    public async Task DownloadDocument_WhenBlobPathEmpty_ReturnsNotFound()
+    {
+        // Arrange
+        var serviceResponse = new ServiceResponse<IActionResult>()
+            .WithContent(new NotFoundObjectResult("File not found"), HttpStatusCode.NotFound);
+
+        Mocker.GetMock<IBlobStorageService>()
+            .Setup(s => s.DownloadFileToHttpResponseAsync(It.IsAny<string>(), string.Empty, It.IsAny<string>()))
+            .ReturnsAsync(serviceResponse);
+
+        // Act
+        var result = await Sut.DownloadDocument("invalid/path", "missing.txt");
+
+        // Assert
+        result.ShouldBeOfType<ObjectResult>();
+    }
+
+    [Fact]
+    public async Task DownloadDocument_WhenFileNameEmpty_ReturnsNotFound()
+    {
+        // Arrange
+        var serviceResponse = new ServiceResponse<IActionResult>()
+            .WithContent(new NotFoundObjectResult("File not found"), HttpStatusCode.NotFound);
+
+        Mocker.GetMock<IBlobStorageService>()
+            .Setup(s => s.DownloadFileToHttpResponseAsync(It.IsAny<string>(), It.IsAny<string>(), string.Empty))
+            .ReturnsAsync(serviceResponse);
+
+        // Act
+        var result = await Sut.DownloadDocument("invalid/path", "missing.txt");
+
+        // Assert
+        result.ShouldBeOfType<ObjectResult>();
+    }
+
+    [Fact]
+    public async Task DownloadDocument_WhenExceptionOccurs_ReturnsObjectResult()
+    {
+        // Arrange
+        var serviceResponse = new ServiceResponse<IActionResult>()
+            .WithContent(new ObjectResult($"Error downloading blob")
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            });
+
+        Mocker.GetMock<IBlobStorageService>()
+            .Setup(s => s.DownloadFileToHttpResponseAsync(It.IsAny<string>(), It.IsAny<string>(), string.Empty))
+            .ReturnsAsync(serviceResponse);
+
+        // Act
+        var result = await Sut.DownloadDocument("invalid/path", "missing.txt");
+
+        // Assert
+        result.ShouldBeOfType<ObjectResult>();
     }
 }
