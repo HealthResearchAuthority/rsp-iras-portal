1598872fb32a33690c0b58c5f29a944242b77315
MateuszBoreckiHRA <mateusz.borecki_pa@hra.nhs.uk>
2025-10-30 14:35:16 +0100

    Changed name of SponsorWorkspaceMenu to SponsorWorkspace

--- src/Web/Rsp.IrasPortal.Web/Features/SponsorWorkspace/Controllers/SponsorWorkspaceController.cs
+++ src/Web/Rsp.IrasPortal.Web/Features/SponsorWorkspace/Controllers/SponsorWorkspaceController.cs
@@ -1,6 +1,9 @@
-﻿using System.Security.Claims;
+﻿using System.Net;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Rsp.IrasPortal.Application.DTOs;
+using Rsp.IrasPortal.Application.Responses;
 using Rsp.IrasPortal.Application.Services;
 using Rsp.IrasPortal.Web.Extensions;
 
@@ -9,12 +12,12 @@ namespace Rsp.IrasPortal.Web.Features.SponsorWorkspace;
 /// <summary>
 /// Controller responsible for handling sponsor workspace related actions.
 /// </summary>
-[Route("sponsorworkspace/[action]", Name = "sws:[action]")]
+[Route("[action]", Name = "sws:[action]")]
 [Authorize(Policy = "IsSponsor")]
-public class SponsorWorkspaceMenuController(IUserManagementService userService) : Controller
+public class SponsorWorkspaceController(IUserManagementService userService) : Controller
 {
     [HttpGet]
-    public async Task<IActionResult> SponsorWorkspaceMenu()
+    public async Task<IActionResult> SponsorWorkspace()
     {
         var currentUserEmail = HttpContext?.User.FindFirstValue(ClaimTypes.Email);
         var userEntityResponse = await userService.GetUser(null, currentUserEmail);
@@ -26,7 +29,14 @@ public class SponsorWorkspaceMenuController(IUserManagementService userService) 
 
         if (string.IsNullOrEmpty(userEntityResponse.Content?.User.Organisation))
         {
-            throw new InvalidOperationException("Sponsor organisation name is missing for the current user.");
+            var errorResponse = new ServiceResponse<UserResponse>()
+                        .WithError(
+                            errorMessage: "Sponsor organisation name is missing for the current user.",
+                            reasonPhrase: "InvalidOperation",
+                            statusCode: HttpStatusCode.BadRequest
+                        );
+
+            return this.ServiceError(errorResponse);
         }
 
         ViewBag.SponsorOrganisationName = userEntityResponse.Content?.User.Organisation;
