13626e89b0d695599f73c513a2eaeb4567b074c5
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-11-03 15:53:27 +0000

    add new "Search Record" page Add unit tests

---
+++ src/Web/Rsp.IrasPortal.Web/Features/Approvals/RecordSearch/Controllers/RecordSearchController.cs
@@ -0,0 +1,51 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Rsp.IrasPortal.Application.Constants;
+using Rsp.IrasPortal.Web.Controllers;
+using Rsp.IrasPortal.Web.Features.Approvals.RecordSearch.Models;
+
+namespace Rsp.IrasPortal.UnitTests.Web.Features.Approvals.ProjectRecordSearch.Controllers;
+
+[Route("[controller]/[action]", Name = "recordsearch:[action]")]
+[Authorize]
+public class RecordSearchController(IValidator<RecordSearchNavigationModel> validator) : Controller
+{
+    [HttpGet("~/[controller]", Name = "recordsearch")]
+    public IActionResult Index()
+    {
+        return View("~/Features/Approvals/RecordSearch/Views/Index.cshtml", new RecordSearchNavigationModel());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Navigate(RecordSearchNavigationModel model)
+    {
+        var context = new ValidationContext<RecordSearchNavigationModel>(model);
+        var validationResult = await validator.ValidateAsync(context);
+
+        if (!validationResult.IsValid)
+        {
+            // Copy the validation results into ModelState.
+            // ASP.NET uses the ModelState collection to populate
+            // error messages in the View.
+            foreach (var error in validationResult.Errors)
+            {
+                ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+            }
+
+            return View("~/Features/Approvals/RecordSearch/Views/Index.cshtml", model);
+        }
+
+        // redirect user based on their selected record type
+        switch (model.RecordType)
+        {
+            case SearchRecordTypes.ProjectRecord:
+                return RedirectToAction(nameof(Index));
+
+            case SearchRecordTypes.ModificationRecord:
+                return RedirectToAction(nameof(ApprovalsController.Index), "Approvals");
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Features/Approvals/RecordSearch/RecordSearchControllerTests.cs
@@ -0,0 +1,92 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Rsp.IrasPortal.Application.Constants;
+using Rsp.IrasPortal.UnitTests.Web.Features.Approvals.ProjectRecordSearch.Controllers;
+using Rsp.IrasPortal.Web.Features.Approvals.RecordSearch.Models;
+
+namespace Rsp.IrasPortal.UnitTests.Web.Features.Approvals.RecordSearch;
+
+public class RecordSearchControllerTests : TestServiceBase<RecordSearchController>
+{
+    [Fact]
+    public void Index_Returns_View()
+    {
+        // arrange
+
+        // act
+        var result = Sut.Index();
+
+        // assert
+        var viewResult = result.ShouldBeOfType<ViewResult>();
+        viewResult.ViewName.ShouldBe("~/Features/Approvals/RecordSearch/Views/Index.cshtml");
+    }
+
+    [Fact]
+    public async Task ProjectRecord_Selection_Redirects_To_ProjectRecord_Search()
+    {
+        // arrange
+        var viewModel = new RecordSearchNavigationModel
+        {
+            RecordType = SearchRecordTypes.ProjectRecord
+        };
+
+        Mocker
+            .GetMock<IValidator<RecordSearchNavigationModel>>()
+            .Setup(v => v.ValidateAsync(It.IsAny<ValidationContext<RecordSearchNavigationModel>>(), default))
+            .ReturnsAsync(new ValidationResult());
+
+        // act
+        var result = await Sut.Navigate(viewModel);
+
+        // assert
+        var viewResult = result.ShouldBeOfType<RedirectToActionResult>();
+        viewResult.ActionName.ShouldBe("Index");
+    }
+
+    [Fact]
+    public async Task ModificationRecord_Selection_Redirects_To_ProjectRecord_Search()
+    {
+        // arrange
+        var viewModel = new RecordSearchNavigationModel
+        {
+            RecordType = SearchRecordTypes.ModificationRecord
+        };
+        Mocker
+            .GetMock<IValidator<RecordSearchNavigationModel>>()
+            .Setup(v => v.ValidateAsync(It.IsAny<ValidationContext<RecordSearchNavigationModel>>(), default))
+            .ReturnsAsync(new ValidationResult());
+
+        // act
+        var result = await Sut.Navigate(viewModel);
+
+        // assert
+        var viewResult = result.ShouldBeOfType<RedirectToActionResult>();
+        viewResult.ActionName.ShouldBe("Index");
+        viewResult.ControllerName.ShouldBe("Approvals");
+    }
+
+    [Fact]
+    public async Task Invalid_SearchRecord_Selection_Redirects_To_ProjectRecord_Search()
+    {
+        // arrange
+        var viewModel = new RecordSearchNavigationModel
+        {
+            RecordType = string.Empty
+        };
+        Mocker
+            .GetMock<IValidator<RecordSearchNavigationModel>>()
+            .Setup(v => v.ValidateAsync(It.IsAny<ValidationContext<RecordSearchNavigationModel>>(), default))
+            .ReturnsAsync(new ValidationResult(new List<ValidationFailure>
+            {
+                new("RecordType", "Required"),
+            }));
+
+        // act
+        var result = await Sut.Navigate(viewModel);
+
+        // assert
+        var viewResult = result.ShouldBeOfType<ViewResult>();
+        viewResult.ViewName.ShouldBe("~/Features/Approvals/RecordSearch/Views/Index.cshtml");
+    }
+}
