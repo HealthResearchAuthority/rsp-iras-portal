f29f2d14a745cd012711f09ca8057dfa1e930ba7
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2025-11-03 16:11:51 +0000

    add more unit tests

---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Validators/SearchRecordNavigationValidatorTests/ValidateAsyncTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation.TestHelper;
+using Rsp.IrasPortal.Web.Features.Approvals.RecordSearch.Models;
+using Rsp.IrasPortal.Web.Validators;
+
+namespace Rsp.IrasPortal.UnitTests.Web.Validators.SearchRecordNavigationValidatorTests;
+
+public class ValidateAsyncTests : TestServiceBase<RecordSearchNavigationModelValidator>
+{
+    [Fact]
+    public async Task ShouldHaveValidationErrorForEmptyRecordType()
+    {
+        // Arrange
+        var model = new RecordSearchNavigationModel
+        {
+            RecordType = null
+        };
+
+        // Act
+        var result = await Sut.TestValidateAsync(model);
+
+        // Assert
+        result
+            .ShouldHaveValidationErrorFor(x => x.RecordType)
+            .WithErrorMessage("Select a record type");
+    }
+}
