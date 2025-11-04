e1b7080e0550277e5f149a9e2541a475b62f0da3
ShilpaHugar <Shilpa.hugar@paconsulting.com>
2025-11-04 12:47:18 +0000

    draft version

---
+++ src/Application/Rsp.IrasPortal.Application/Constants/ModificationOverview.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rsp.IrasPortal.Application.Constants;
+
+public static class ModificationOverview
+{
+    public const string SponserDetails = "Sponsor details";
+    public const string ModificationDetails = "Modification details";
+    public const string Documents = "Documents";
+    public const string History = "History";
+}
