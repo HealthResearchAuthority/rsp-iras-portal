2a169de948990678da52ec352cf1f675646ba099
ShilpaHugar <Shilpa.hugar@paconsulting.com>
2025-11-10 09:18:51 +0000

    typo correction

--- src/Application/Rsp.IrasPortal.Application/Constants/ModificationOverview.cs
+++ src/Application/Rsp.IrasPortal.Application/Constants/ModificationOverview.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Rsp.IrasPortal.Application.Constants;
+﻿namespace Rsp.IrasPortal.Application.Constants;
 
 public static class ModificationOverview
 {
-    public const string SponserDetails = "Sponsor details";
+    public const string SponsorDetails = "Sponsor details";
     public const string ModificationDetails = "Modification details";
     public const string Documents = "Documents";
     public const string History = "History";
