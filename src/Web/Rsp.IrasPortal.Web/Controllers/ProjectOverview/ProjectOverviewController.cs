cb5eb4f9a66bbeb9bc6ba0d79c5e569b930788a6
ShilpaHugar <Shilpa.hugar@paconsulting.com>
2025-11-25 13:28:03 +0000

    Bugs fix: RSP-6272, RSP-5285, RSP-5571, RSP-5572, RSP-6277

--- src/Web/Rsp.IrasPortal.Web/Controllers/ProjectOverview/ProjectOverviewController.cs
+++ src/Web/Rsp.IrasPortal.Web/Controllers/ProjectOverview/ProjectOverviewController.cs
@@ -74,8 +74,8 @@ public class ProjectOverviewController(
         string? backRoute,
         int pageNumber = 1,
         int pageSize = 20,
-        string sortField = nameof(ModificationsModel.ModificationNumber),
-        string sortDirection = SortDirections.Descending
+        string? sortField = null,
+        string? sortDirection = null
     )
     {
         UpdateModificationRelatedTempData();
@@ -127,7 +127,13 @@ public class ProjectOverviewController(
                 Status = dto.Status,
             })
             .ToList() ?? [];
-
+        if (string.IsNullOrEmpty(sortField) && string.IsNullOrEmpty(sortDirection))
+        {
+            model.Modifications = model.Modifications.OrderBy(item => Enum.TryParse<ModificationStatusOrder>(GetEnumStatus(item.Status!), true, out var statusEnum)
+                    ? (int)statusEnum
+                    : (int)ModificationStatusOrder.None)
+                .ToList() ?? [];
+        }
         model.Pagination = new PaginationViewModel(pageNumber, pageSize, modificationsResponseResult?.Content?.TotalCount ?? 0)
         {
             SortDirection = sortDirection,
