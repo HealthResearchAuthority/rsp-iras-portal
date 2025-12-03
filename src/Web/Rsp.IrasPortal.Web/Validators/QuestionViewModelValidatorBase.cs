76e274dc41dc87e10a60eb44d62413e1fbbe3eb6
MateuszBoreckiHRA <mateusz.borecki_pa@hra.nhs.uk>
2025-12-03 12:01:10 +0100

    5747 - issues 1-10

--- src/Web/Rsp.IrasPortal.Web/Validators/QuestionViewModelValidatorBase.cs
+++ src/Web/Rsp.IrasPortal.Web/Validators/QuestionViewModelValidatorBase.cs
@@ -23,6 +23,21 @@ public class QuestionViewModelValidatorBase : AbstractValidator<QuestionViewMode
                     // split the value to get the min max
                     var minmax = condition.Value?.Split(',', StringSplitOptions.RemoveEmptyEntries);
 
+                    // empty check
+                    if (minmax?.Length == 1)
+                    {
+                        // perform empty answer check
+                        if (minmax[0].Contains("EMPTY") && string.IsNullOrEmpty(question.AnswerText?.Replace("\r\n", "\n")))
+                        {
+                            // by setting IsApplicable property
+                            // it will display the Description of the condition
+                            // for the property
+                            condition.IsApplicable = true;
+                            context.AddFailure(nameof(question.AnswerText), $"{condition.Description}");
+                            break; // Exit the loop immediately if EMPTY check fails
+                        }
+                    }
+
                     // check if we have an array of length 2
                     if (minmax?.Length == 2)
                     {
