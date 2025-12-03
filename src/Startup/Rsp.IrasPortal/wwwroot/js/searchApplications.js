5f95b983b49ef771a868086c82e509cd1ee8f93a
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-12-03 13:41:54 +0000

    RSP-5909 Q7.S4 - Team 2 - Cosmetic defects

--- src/Startup/Rsp.IrasPortal/wwwroot/js/searchApplications.js
+++ src/Startup/Rsp.IrasPortal/wwwroot/js/searchApplications.js
@@ -1,3 +1,5 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
     initCheckboxCount("Search.Status", "status-hint");
+        initCheckboxCount("Search.LeadNation", "lead-nation-hint");
+        initCheckboxCount("Search.ParticipatingNation", "participating-nation-hint");
 });
