0d2b9ddee7b2a1dffdb47c3259ea3955b415e277
Shahzad Hassan <shahzad.hassan@paconsulting.com>
2025-12-11 16:41:57 +0000

    RSP-6349: Use managed IDs for Validate IrasId Function - Updated AppSettings to include Managed Identity settings - Refactored FunctionKeyHeadersHandler to FunctionHeadersHandler   to support managed identity authentication - Updated HttpClient configurations to only register FunctionHeadersHandler   if it's not development environment

--- src/Startup/Rsp.IrasPortal/Program.cs
+++ src/Startup/Rsp.IrasPortal/Program.cs
@@ -86,7 +86,7 @@ services.AddServices();
 
 services.AddHttpContextAccessor();
 
-services.AddHttpClients(appSettings!);
+services.AddHttpClients(appSettings!, builder.Environment);
 
 // routing configuration
 services.AddRouting(options => options.LowercaseUrls = true);
