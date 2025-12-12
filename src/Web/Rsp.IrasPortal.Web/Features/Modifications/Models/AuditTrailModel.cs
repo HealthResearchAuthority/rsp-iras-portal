47507b051f0abbec122c22db13982b152ffef061
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-12-12 13:16:47 +0000

    RSP-6445 Added breadcrumb for modification audit trail

--- src/Web/Rsp.IrasPortal.Web/Features/Modifications/Models/AuditTrailModel.cs
+++ src/Web/Rsp.IrasPortal.Web/Features/Modifications/Models/AuditTrailModel.cs
@@ -7,4 +7,5 @@ public class AuditTrailModel
     public ProjectModificationAuditTrailResponse AuditTrail { get; set; } = null!;
     public string ModificationIdentifier { get; set; } = null!;
     public string ShortTitle { get; set; } = null!;
+    public string ProjectRecordId { get; set; } = null!;
 }
