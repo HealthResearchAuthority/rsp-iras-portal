241ab1009deed38ccfd2d0e6be3feb249a215218
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-12-22 11:39:25 +0000

    RSP-6420 Added tab component and partial views for linked tickets

---
+++ src/Web/Rsp.IrasPortal.Web/Features/SponsorWorkspace/MyOrganisations/Models/SponsorMyOrganisationProfileViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Rsp.IrasPortal.Application.DTOs;
+using Rsp.IrasPortal.Web.Areas.Admin.Models;
+
+namespace Rsp.IrasPortal.Web.Features.SponsorWorkspace.MyOrganisations.Models;
+
+public class SponsorMyOrganisationProfileViewModel
+
+{
+}
