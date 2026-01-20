b7872547331ca299b4f3519aa5be851e1de4b433
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2026-01-20 09:46:35 +0000

    remove unused file

--- src/Application/Rsp.IrasPortal.Application/Constants/SponsorOrganisationUserRoles.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Rsp.Portal.Application.Constants;
-
-//public static class SponsorOrganisationUserRoles
-//{
-//    public const string Sponsor = "sponsor";
-//    public const string OrganisationAdministrator = "organisation_administrator";
-//}
