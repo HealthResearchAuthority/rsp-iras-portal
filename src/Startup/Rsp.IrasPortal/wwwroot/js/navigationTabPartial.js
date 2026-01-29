ac1120674cf806b63c2cbf3b9f777ea79e635cb8
MateuszBoreckiHRA <mateusz.borecki_pa@hra.nhs.uk>
2026-01-29 14:14:11 +0100

    Change var to let/const according to sonar

--- src/Startup/Rsp.IrasPortal/wwwroot/js/navigationTabPartial.js
+++ src/Startup/Rsp.IrasPortal/wwwroot/js/navigationTabPartial.js
@@ -1,22 +1,22 @@
 ﻿/// <reference path="../lib/jquery/dist/jquery.js" />
 
 $(function () {
-    var scrollKey = 'scroll:' + location.pathname;
+    const scrollKey = 'scroll:' + location.pathname;
 
     // 1) Save scroll when a tab link is clicked
     $('a.govuk-service-navigation__link[data-tab]').on('click', function () {
-        var y = $(window).scrollTop();
+        let y = $(window).scrollTop();
         localStorage.setItem(scrollKey, String(y));
     });
 
     // 2) Safety net: also save before unload (covers refresh or non-tab navigation)
     $(window).on('beforeunload', function () {
-        var y = $(window).scrollTop();
+        let y = $(window).scrollTop();
         localStorage.setItem(scrollKey, String(y));
     });
 
     // 3) Restore on load
-    var y = Number(localStorage.getItem(scrollKey) || 0);
+    let y = Number(localStorage.getItem(scrollKey) || 0);
 
     if (y > 0) {
         $(window).scrollTop(y);
