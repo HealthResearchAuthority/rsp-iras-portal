1c6846e707e884f8edb6ce2fc40919f537841c6b
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2026-04-17 16:14:05 +0100

    Add/update rec member

---
+++ src/Application/Rsp.IrasPortal.Application/Constants/AddRecMemberFields.cs
@@ -0,0 +1,20 @@
+﻿namespace Rsp.IrasPortal.Application.Constants;
+
+public static class AddRecMemberFields
+{
+    public static readonly List<string> CommitteeRoles =
+   [
+       "Alternate vide chair",
+        "Member",
+        "Vice chair",
+        "Chair"
+   ];
+
+    public static readonly List<string> Desginations =
+   [
+       "Health care member",
+        "Member",
+        "Lay member",
+        "Social care member"
+   ];
+}
