a0f3ca9a90aa759fca2f34f4cb3764a318e428ec
Borjan Vasovski <borjan.vasovski_PA@hra.nhs.uk>
2026-04-20 10:45:54 +0100

    Increase test coverage

---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Features/MemberManagement/RecMemberManagementControllerTests/EditRecMemberTests.cs
@@ -0,0 +1,223 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Rsp.IrasPortal.Web.Features.MemberManagement.Controllers;
+using Rsp.IrasPortal.Web.Features.MemberManagement.Models;
+using Rsp.Portal.Application.Constants;
+using Rsp.Portal.Application.DTOs;
+using Rsp.Portal.Application.Responses;
+using Rsp.Portal.Application.Services;
+using Rsp.Portal.Domain.Identity;
+using Rsp.Portal.UnitTests;
+
+namespace Rsp.IrasPortal.UnitTests.Web.Features.MemberManagement.RecMemberManagementControllerTests;
+
+public class EditRecMemberTests : TestServiceBase<RecMemberManagementController>
+{
+    private readonly DefaultHttpContext _http;
+
+    public EditRecMemberTests()
+    {
+        var tempData = new TempDataDictionary(new DefaultHttpContext(), Mock.Of<ITempDataProvider>());
+        Sut.TempData = tempData;
+        _http = new DefaultHttpContext { Session = new InMemorySession() };
+        Sut.ControllerContext = new ControllerContext { HttpContext = _http };
+    }
+
+    [Theory, AutoData]
+    public async Task EditRecMember_Returns_For_HappyPath_View(Guid recId,
+      ReviewBodyDto reviewBody,
+      AddRecMemberViewModel viewModel,
+      Guid userId)
+    {
+        var userEmail = "user@example.com";
+        var user = new UserResponse
+        {
+            User = new User(userId.ToString(),
+                            "azure-ad-12345",
+                            "Mr",
+                            "Test",
+                            "Test",
+                            userEmail,
+                            "Software Developer",
+                            "orgName", // IMPORTANT: match org if your action filters by org
+                            "+44 7700 900123",
+                            "United Kingdom",
+                            IrasUserStatus.Active,
+                            DateTime.UtcNow,
+                            DateTime.UtcNow.AddDays(-2),
+                            DateTime.UtcNow)
+        };
+
+        viewModel.Email = userEmail;
+        viewModel.RecId = recId;
+        viewModel.RecName = reviewBody.RegulatoryBodyName;
+
+        reviewBody.Id = recId;
+        reviewBody.Users = new List<ReviewBodyUserDto>
+        {
+            new ReviewBodyUserDto
+            {
+                UserId = userId,
+                Id = recId,
+                Email = userEmail
+            }
+        };
+
+        // arrange
+        var userResponse = new ServiceResponse<UserResponse>
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = user
+        };
+
+        Mocker.GetMock<IUserManagementService>()
+            .Setup(s => s.GetUser(userId.ToString(), null, null))
+            .ReturnsAsync(userResponse);
+
+        var reviewBodyResponse = new ServiceResponse<ReviewBodyDto>
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = reviewBody
+        };
+
+        Mocker.GetMock<IReviewBodyService>()
+            .Setup(s => s.GetReviewBodyById(recId))
+            .ReturnsAsync(reviewBodyResponse);
+
+        // act
+        var result = await Sut.EditRecMember(recId, userId);
+
+        // assert
+        var redirectResult = result.ShouldBeOfType<ViewResult>();
+        var model = redirectResult.Model.ShouldBeAssignableTo<RecMemberViewModel>();
+        model.ShouldNotBeNull();
+        model.IsEditMode.ShouldBeTrue();
+        model.UserId.ShouldBe(user.User.Id);
+        model.RecId.ShouldBe(recId);
+        model.EmailAddress.ShouldBe(user.User.Email);
+
+        Mocker.GetMock<IReviewBodyService>()
+            .Verify(s => s.GetReviewBodyById(recId), Times.Once);
+        Mocker.GetMock<IUserManagementService>()
+            .Verify(s => s.GetUser(userId.ToString(), null, null), Times.Once);
+    }
+
+    [Theory, AutoData]
+    public async Task EditRecMember_Returns_Error_When_User_Is_Null(
+        Guid recId,
+        Guid userId,
+        ReviewBodyDto reviewBody)
+    {
+        var userEmail = "user@example.com";
+        reviewBody.Id = recId;
+        reviewBody.Users = new List<ReviewBodyUserDto>
+        {
+            new ReviewBodyUserDto
+            {
+                UserId = userId,
+                Id = recId,
+                Email = userEmail
+            }
+        };
+        var reviewBodyResponse = new ServiceResponse<ReviewBodyDto>
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = reviewBody
+        };
+
+        Mocker.GetMock<IReviewBodyService>()
+            .Setup(s => s.GetReviewBodyById(recId))
+            .ReturnsAsync(reviewBodyResponse);
+
+        var userResponse = new ServiceResponse<UserResponse>
+        {
+            StatusCode = HttpStatusCode.BadGateway,
+            Content = null
+        };
+
+        Mocker.GetMock<IUserManagementService>()
+            .Setup(s => s.GetUser(userId.ToString(), null, null))
+            .ReturnsAsync(userResponse);
+
+        // act
+        var result = await Sut.EditRecMember(recId, userId);
+
+        // assert
+        var redirectResult = result.ShouldBeOfType<StatusCodeResult>();
+        redirectResult.StatusCode.ShouldBe((int)HttpStatusCode.BadGateway);
+
+        Mocker.GetMock<IReviewBodyService>()
+            .Verify(s => s.GetReviewBodyById(recId), Times.Once);
+        Mocker.GetMock<IUserManagementService>()
+            .Verify(s => s.GetUser(userId.ToString(), null, null), Times.Once);
+    }
+
+    [Theory, AutoData]
+    public async Task EditRecMember_Returns_Error_When_User_Does_Is_Not_Member(
+        Guid recId,
+        Guid userId,
+        ReviewBodyDto reviewBody)
+    {
+        reviewBody.Id = recId;
+        reviewBody.Users = new List<ReviewBodyUserDto>();
+        var reviewBodyResponse = new ServiceResponse<ReviewBodyDto>
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = reviewBody
+        };
+
+        Mocker.GetMock<IReviewBodyService>()
+            .Setup(s => s.GetReviewBodyById(recId))
+            .ReturnsAsync(reviewBodyResponse);
+
+        var userResponse = new ServiceResponse<UserResponse>
+        {
+            StatusCode = HttpStatusCode.BadGateway,
+            Content = null
+        };
+
+        Mocker.GetMock<IUserManagementService>()
+            .Setup(s => s.GetUser(userId.ToString(), null, null))
+            .ReturnsAsync(userResponse);
+
+        // act
+        var result = await Sut.EditRecMember(recId, userId);
+
+        // assert
+        var redirectResult = result.ShouldBeOfType<NotFoundResult>();
+        redirectResult.StatusCode.ShouldBe((int)HttpStatusCode.NotFound);
+
+        Mocker.GetMock<IReviewBodyService>()
+            .Verify(s => s.GetReviewBodyById(recId), Times.Once);
+        Mocker.GetMock<IUserManagementService>()
+            .Verify(s => s.GetUser(userId.ToString(), null, null), Times.Never);
+    }
+
+    [Theory, AutoData]
+    public async Task EditRecMember_Returns_Error_When_Review_Body_Is_Null(Guid recId,
+      Guid userId)
+    {
+        var reviewBodyResponse = new ServiceResponse<ReviewBodyDto>
+        {
+            StatusCode = HttpStatusCode.BadGateway,
+            Content = null
+        };
+
+        Mocker.GetMock<IReviewBodyService>()
+            .Setup(s => s.GetReviewBodyById(recId))
+            .ReturnsAsync(reviewBodyResponse);
+
+        // act
+        var result = await Sut.EditRecMember(recId, userId);
+
+        // assert
+        var redirectResult = result.ShouldBeOfType<StatusCodeResult>();
+        redirectResult.StatusCode.ShouldBe((int)HttpStatusCode.BadGateway);
+
+        Mocker.GetMock<IReviewBodyService>()
+            .Verify(s => s.GetReviewBodyById(recId), Times.Once);
+        Mocker.GetMock<IUserManagementService>()
+            .Verify(s => s.GetUser(userId.ToString(), null, null), Times.Never);
+    }
+}
---
+++ tests/UnitTests/Rsp.IrasPortal.UnitTests/Web/Validators/AddRecMemberModelValidatorTests/ValidateAsyncTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentValidation.TestHelper;
+using Rsp.IrasPortal.Web.Features.MemberManagement.Models;
+using Rsp.IrasPortal.Web.Validators;
+
+namespace Rsp.Portal.UnitTests.Web.Validators.AddRecMemberModelValidatorsTests;
+
+public class ValidateAsyncTests : TestServiceBase<AddRecMemberModelValidator>
+{
+    [Fact]
+    public async Task ShouldHaveValidationErrorForEmailAddressEmpty()
+    {
+        // Arrange
+        var model = new AddRecMemberViewModel
+        {
+            Email = null!
+        };
+
+        // Act
+        var result = await Sut.TestValidateAsync(model);
+
+        // Assert
+        result
+            .ShouldHaveValidationErrorFor(x => x.Email)
+            .WithErrorMessage("Enter an email address in the correct format, like name@example.com");
+    }
+
+    [Fact]
+    public async Task ShouldHaveValidationErrorForInvalidEmailAddress()
+    {
+        // Arrange
+        var model = new AddRecMemberViewModel
+        {
+            Email = "1245uuu1111122"
+        };
+
+        // Act
+        var result = await Sut.TestValidateAsync(model);
+
+        // Assert
+        result
+            .ShouldHaveValidationErrorFor(x => x.Email)
+            .WithErrorMessage("Enter an email address in the correct format, like name@example.com");
+    }
+
+    [Fact]
+    public async Task ShouldHaveValidationErrorForLongEmailAddress()
+    {
+        // Arrange
+        var model = new AddRecMemberViewModel
+        {
+            Email = "wewewewewefaddddddddddddddddddddddddddddddweweweweweweweweweweweweweweweweweweweweweweweweedddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrhellooooowijrieoifneuiofnieufnieufnbiuebfiuwbefuiwebfiuwefg@google.com"
+        };
+
+        // Act
+        var result = await Sut.TestValidateAsync(model);
+
+        // Assert
+        result
+            .ShouldHaveValidationErrorFor(x => x.Email)
+            .WithErrorMessage("Email address must be 254 characters or less");
+    }
+}
