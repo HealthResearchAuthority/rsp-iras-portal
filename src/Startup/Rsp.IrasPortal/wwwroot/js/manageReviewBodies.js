574ce8ab4be4fe56fad7a184b95ba131a88906ac
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2026-04-28 10:49:29 +0100

    Add checkbox count back

--- src/Startup/Rsp.IrasPortal/wwwroot/js/manageReviewBodies.js
+++ src/Startup/Rsp.IrasPortal/wwwroot/js/manageReviewBodies.js
@@ -1,4 +1,8 @@
-﻿// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    initCheckboxCount("Search.Country", "country-hint");
+});
+
+// for details on configuring this project to bundle and minify static web assets.
 $(function () {
     function toggleConditionalField() {
         // Hide all conditional fields first
